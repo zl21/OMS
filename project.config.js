1e645ea49ddbf4e1ee483bb52afaf64e55bcd6ff
sq <sun.qian@burgeon.cn>
2020-09-15 09:44:07 +0800

    更换网关

--- project.config.js
+++ project.config.js
@@ -3,9 +3,9 @@ const path = require('path')
 module.exports = {
   projectsTitle: 'Your Project Name', // 项目title
   qiaochuTitle: '巧厨电商',
-  projectIconPath: path.join(__dirname, './favicon.png'), // 项目icon,
+  projectIconPath: path.join(__dirname, './favicon.ico'), // 项目icon,
   // target: 'http://r3-8098.dev.burgeononline.com/', // 项目后端服务地址
   // target: 'http://106.15.154.165:28888', // 项目后端服务地址(千百度)
   // target: ' http://r3dev.qiaodan.com:26666', // 项目后端服务地址(乔丹)
-  target: ' http://47.100.164.193:26666', // 项目后端服务地址(巧厨)
+  target: ' http://47.103.6.45:27777', // 项目后端服务地址(森马)
 }
