d42c075b4f006d3a1ca7115f4ed2e1f2ac01b96a
weixiaoxiao <1655654318@qq.com>
2020-09-29 18:43:23 +0800

    订单中心定制弹框 旗帜修改图片引入方式

--- src/views/modal/orderCenter/changeRemark.vue
+++ src/views/modal/orderCenter/changeRemark.vue
@@ -3,23 +3,8 @@
     <Form :label-width="80" :model="formItem">
       <FormItem label="旗帜:">
         <RadioGroup @on-change="radioChange" v-model="formItem.flag">
-          <Radio label="1">
-            <img src="@/assets/image/img/0.png" alt />
-          </Radio>
-          <Radio label="2">
-            <img src="@/assets/image/img/2.png" alt />
-          </Radio>
-          <Radio label="3">
-            <img src="@/assets/image/img/3.png" alt />
-          </Radio>
-          <Radio label="4">
-            <img src="@/assets/image/img/4.png" alt />
-          </Radio>
-          <Radio label="5">
-            <img src="@/assets/image/img/5.png" alt />
-          </Radio>
-          <Radio label="0">
-            <img src="@/assets/image/img/0.png" alt />
+          <Radio :label="index"  v-for="index of 5" :key="index">
+            <img :src="'@/assets/image/img/'+ index +'.png'" alt />
           </Radio>
         </RadioGroup>
       </FormItem>
