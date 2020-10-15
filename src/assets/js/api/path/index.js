ae276182d8566f954ad279da409e6350bb8fd358
xuhui <xu.h@burgeon.cn>
2020-10-15 20:04:43 +0800

    修复部分页面组件属性$httpApi报undefind

---
+++ src/assets/js/api/path/index.js
@@ -0,0 +1,9 @@
+import order from './order'
+import strategyPlatform from './strategyPlatform'
+import publicPath from './publicPath'
+
+export default {
+  order,
+  strategyPlatform,
+  public: publicPath
+}
