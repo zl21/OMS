9ab2f42e71bcb3cf5905f501396e414cbd6291a8
sunny <sun.qian@burgeon.cn>
2020-10-22 17:17:43 +0800

    更改服务

--- project.config.js
+++ project.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   projectsTitle: 'Your Project Name', // 项目title
@@ -7,8 +7,8 @@ module.exports = {
   // target: 'http://r3-8098.dev.burgeononline.com/', // 项目后端服务地址
   // target: 'http://106.15.154.165:28888', // 项目后端服务地址(千百度)
   // target: ' http://r3dev.qiaodan.com:26666', // 项目后端服务地址(乔丹)
-  target: 'http://47.103.6.45:26666', // 项目后端服务地址(森马)
-}
+  target: 'http://139.224.134.69', // 项目后端服务地址(森马)
+};
 /**
  * 关于国际化
  * 1.使用说明请参见依赖包中的README.md文档(node_modules/@burgeon/internationalization/README.md)
