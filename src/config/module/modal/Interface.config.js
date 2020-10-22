05b462ad43976a593c1df14856b2176f5c895740
weixiaoxiao <1655654318@qq.com>
2020-10-22 14:11:47 +0800

    弹框配置修改

--- src/config/module/modal/Interface.config.js
+++ src/config/module/modal/Interface.config.js
@@ -1,13 +1,13 @@
 // 接口平台
 export default {
   // 淘宝订单接口
-  downLoadTaobaoOrder: {
+  DOWNLOADORDER: {
     component: () => {
       import("@/views/modal/InterfacePlatform/downLoadAll.vue");
     },
   },
   // 通用订单接口 通用退单接口 (通用订单下载)
-  downLoadPublic : {
+  DOWNLOADPUBLIC: {
     component: () => {
       import("@/views/modal/InterfacePlatform/downLoadPublic.vue");
     }
--- src/config/module/modal/commodity.config.js
+++ src/config/module/modal/commodity.config.js
@@ -1,13 +1,21 @@
 // 商品中心
 export default {
+  // //淘宝商品 (下载商品)
+  DOWNLOADTAOBAOGOODS: {
+    component: () => import("@/views/modal/commodityCenter/downLoadTaobaoGoods.vue"),
+  },
   // //京东商品 (下载商品)
-  downLoadJingdongGoods: {
+  DOWNLOADJINGDONGGOODS: {
     component: () => import("@/views/modal/commodityCenter/downLoadJingdongGoods.vue"),
   },
-  // 通用商品(通用商品下载)
-  downLoadPublic : {
-    component: () => {
-      import("@/views/modal/InterfacePlatform/downLoadPublic.vue");
-    }
+  // //京东商品 (下载商品)
+  DOWNLOADVIPGOODS: {
+    component: () => import("@/views/modal/commodityCenter/downLoadVipGoods.vue"),
   },
+  // 通用商品(通用商品下载)  同接口平台通用订单接口
+  // DOWNLOADPUBLIC: {
+  //   component: () => {
+  //     import("@/views/modal/InterfacePlatform/downLoadPublic.vue");
+  //   }
+  // },
 };
