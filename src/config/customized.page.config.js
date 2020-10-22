625ac12c2293a3108ecf23d92036b9d78c28c0b7
zhou.l <zhou.l@burgeon.cn>
2020-10-22 18:43:05 +0800

    //组织中心

--- src/config/customized.page.config.js
+++ src/config/customized.page.config.js
@@ -3,7 +3,7 @@ import MarketCenter from './module/page/promotion.config.js'; // ------------促
 import OrdersCenter from './module/page/order.config.js'; // ------------订单中心
 import FinanceCenter from "./module/page/finance.config.js"; //-----------财务中心
 import CommodityCenter from "./module/page/commodity.config.js"; //-----------商品中心
-import OrganizationCenter from "./module/page/organization.config.js"; //-----------组织中心
+// import OrganizationCenter from "./module/page/organization.config.js"; //-----------组织中心
 import StrategyPlatform from "./module/page/strategy.config.js"; //-----------策略平台
 import SystemConfig from './module/page/system.config.js'; // -----------系统配置
 import InventoryConfig from './module/page/inventory.config.js';// -----------库存中心
@@ -12,7 +12,7 @@ const all = Object.assign(OrdersCenter,
   MarketCenter,
   FinanceCenter,
   CommodityCenter,
-  OrganizationCenter,
+  // OrganizationCenter,
   StrategyPlatform,
   SystemConfig,
   InventoryConfig);
