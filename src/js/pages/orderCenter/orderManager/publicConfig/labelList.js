7b4f6a755ce3b5b4c69091e431ff8e3676bb23a4
weixiaoxiao <1655654318@qq.com>
2020-10-26 15:40:16 +0800

    拆分定制页面逻辑层

---
+++ src/js/pages/orderCenter/orderManager/publicConfig/labelList.js
@@ -0,0 +1,71 @@
+export default [{
+    label: "全部",
+    value: "0",
+    isShow: true
+  },
+  {
+    label: "待审核",
+    value: "1",
+    isShow: true
+  },
+  {
+    label: "缺货",
+    value: "2",
+    isShow: true
+  },
+  {
+    label: "已审核",
+    value: "3",
+    isShow: true
+  },
+  {
+    label: "配货中",
+    value: "4",
+    isShow: true
+  },
+  {
+    label: "仓库发货",
+    value: "5",
+    isShow: true
+  },
+  {
+    label: "平台发货",
+    value: "6",
+    isShow: true
+  },
+  // {
+  //   label: "已确认收货",
+  //   value: "11",
+  //   isShow: true
+  // }, 
+  {
+    label: "取消/作废",
+    value: "7,8",
+    isShow: true
+  },
+  // {
+  //   label: "系统作废",
+  //   value: "8",
+  //   isShow: true
+  // },
+  // {
+  //   label: "交易完成",
+  //   value: "12",
+  //   isShow: true
+  // },
+  // {
+  //   label: "预售待发货",
+  //   value: "51",
+  //   isShow: true
+  // },
+  // {
+  //   label: "预售缺货",
+  //   value: "52",
+  //   isShow: true
+  // }
+  {
+    label: "待分配",
+    value: "50",
+    isShow: true
+  }
+]
