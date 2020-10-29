32b79188f2d393dcb280246577856e113f196dca
段若愚 <duan.ry@burgeon.cn>
2020-10-29 18:54:32 +0800

    1.  修改项目配置文件;

--- src/config/customized.theme.js
+++ src/config/customized.theme.js
@@ -1,12 +1,13 @@
 export default {
   // 动态创建link标签
+  // url: require("@/node_modules/@burgeon/oms-theme/skin/skyBlue/index.min.css"),
   appendLink: skinName => {
-    let url = `/node_modules/@burgeon/oms-theme/skin/${skinName}/index.min.css`;
+    // let url = require(`/node_modules/@burgeon/oms-theme/skin/${skinName}/index.min.css`);
     var link = document.createElement("link");
     link.setAttribute("id", "skin");
     link.setAttribute("type", "text/css");
     link.setAttribute("rel", "stylesheet");
-    link.setAttribute("href", url);
+    link.setAttribute("href", "/node_modules/@burgeon/oms-theme/skin/skyBlue/index.min.css");
     document.head.appendChild(link);
   }
 };
