a239ca7e3a4c5a490eddcc5f48c9c5e19d75e0cd
weixiaoxiao <1655654318@qq.com>
2020-11-02 13:43:39 +0800

    订单管理拆分逻辑样式  --- common 文件夹（遗漏）

--- src/assets/js/api/path/index.js
+++ src/assets/js/api/path/index.js
@@ -1,9 +1,7 @@
 import order from './order'
-import strategyPlatform from './strategyPlatform'
 import publicPath from './publicPath'
 
 export default {
   order,
-  strategyPlatform,
   public: publicPath
 }
