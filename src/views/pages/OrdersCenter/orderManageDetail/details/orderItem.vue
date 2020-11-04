b75fc33d78f99677f90a13b248d845806caee3b7
sunny <sun.qian@burgeon.cn>
2020-11-04 11:40:06 +0800

    部分表格组件名不匹配

--- src/views/pages/OrdersCenter/orderManageDetail/details/orderItem.vue
+++ src/views/pages/OrdersCenter/orderManageDetail/details/orderItem.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <jordan-action-table
-      :jordanTableConfig="tableConfig"
+    <business-action-table
+      :jordan-table-config="tableConfig"
       @on-page-change="pageChange"
       @on-page-size-change="pageSizeChange"
-    ></jordan-action-table>
+    />
   </div>
 </template>
 
 <script>
-  import orderItem from "@/js/pages/orderCenter/orderManageDetail/details/orderItem";
+  import orderItem from '@/js/pages/orderCenter/orderManageDetail/details/orderItem';
+
   export default orderItem;
 </script>
