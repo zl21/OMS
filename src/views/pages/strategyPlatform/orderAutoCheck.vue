017a468710eb02715df5302427f1ddde3e8cf416
sunny <sun.qian@burgeon.cn>
2020-11-05 20:19:10 +0800

    订单自动审核hold等待时间功能

--- src/views/pages/strategyPlatform/orderAutoCheck.vue
+++ src/views/pages/strategyPlatform/orderAutoCheck.vue
@@ -52,6 +52,31 @@
                       </Col>
                     </FormItem>
                   </Col>
+                  <Col span="4">
+                    <FormItem label="是否可合并：">
+                      <Checkbox
+                        @on-change="setResult('IS_MERGE_ORDER')"
+                        size="small"
+                        v-model="IS_MERGE_ORDER"
+                      >&nbsp;
+                      </Checkbox>
+                    </FormItem>
+                  </Col>
+                  <Col span="8">
+                    <FormItem label="hold单等待时间：">
+                      <Col span="20">
+                        <Input
+                          size="small"
+                          v-model="info.HOLD_WAIT_TIME"
+                          @on-change="setResult('HOLD_WAIT_TIME')"
+                          :maxlength="5"
+                        />
+                      </Col>
+                      <Col span="4">
+                        分钟
+                      </Col>
+                    </FormItem>
+                  </Col>
                 </Row>
               </div>
             </Panel>
