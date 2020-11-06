b6bac839b1a562fb6eb991a7a0f9c2711c0cfe79
weixiaoxiao <1655654318@qq.com>
2020-11-06 19:45:50 +0800

    促销中心 - 新增编辑 - 商品池设置页面拆分

---
+++ src/js/pages/promotionCenter/details/setCommodity.js
@@ -0,0 +1,148 @@
+import detailtable from "@/views/pages/promotionCenter/details/table.vue";
+import tableCols from "@/assets/js/promotion/columns.js";
+import MyDialog from 'framework/components/dialog/mydialog'
+ export default {
+     name:"dialogVisiblex",
+     data(){
+         return{
+           tableCols: tableCols,
+           commodityList:{
+                current: 1,
+                total: 0,
+                pageSize: 10,
+                data: []
+           }
+         }
+     },
+    props:{
+        dialog_visible: {
+        type: Boolean,
+        default: false
+        },
+        giftData: {
+        type: Object
+        },
+        basicData: {
+            type: Object
+        },
+        objid:{
+        type: String
+        },
+        loadDis: {
+        type: Boolean,
+        default: false
+        },
+    },
+    components: {
+        detailtable,
+        MyDialog
+    },
+    computed:{
+        columns() {
+            return this.tableCols.set_commodity;
+        },
+    },
+    watch:{
+        loadDis: function(val) {
+            if (val === true) {
+                this.commodityList.data = []
+                this.initView();
+            }
+         }
+    },
+    mounted(){
+    },
+    created(){
+    },
+    activated(){
+
+    },
+    methods:{
+        /**
+         * 根据数据源 真实展示数据
+         */
+        initView() {
+            let obj = {
+                current: 1,
+                total: 0,
+                pageSize: 10,
+                data: []
+            };
+            this.commodityList = obj;
+            this.tablelistView(
+            this.giftData.gift_commoditylist,
+            this.commodityList
+            );
+        },
+        confirm(){
+            this.$emit('confirm')
+        },
+        closeDialogs(){
+            this.$emit('closeDialog')
+        },
+        /*** 根据当前页和当前页数目 ，展示表格
+         * @row 原始表格数据
+         * @obj 表格虚拟视图
+         */
+        tablelistView(rows, obj) {
+        let current = obj.current || 1;
+        let pageSize = obj.pageSize || 10;
+        let start = Number((current - 1) * pageSize);
+        let end = Number(current * pageSize);
+        obj.total = rows.length;
+        obj.data = rows.slice(start, end);
+        },
+        pageChange(val) {
+        this.commodityList.current = val;
+        this.tablelistView(
+            this.giftData.gift_commoditylist,
+            this.commodityList
+        );
+        },
+        /**
+         * 切换非tab页面的表格的页长度
+         */
+        onPageSizeChange(val) {
+        this.commodityList.pageSize = val;
+        this.tablelistView(
+            this.giftData.gift_commoditylist,
+            this.commodityList
+        );
+        },
+        /**
+         * 单表格添加和删除 灵活展示表格
+         */
+        countTablelistView() {
+        let rows = this.giftData.gift_commoditylist || [];
+        let obj = this.commodityList;
+        let pageSize = obj.pageSize || 10;
+        let pagesLen = Math.ceil(rows.length / pageSize);
+        if (obj.current > pagesLen) obj.current = pagesLen;
+        if (pagesLen === 0) obj.current = 1;
+        this.tablelistView(rows, this.commodityList);
+        },
+        /**
+         * 删除多余属性
+         */
+        deleteProperty(row) {
+        delete row._index;
+        delete row.colspan;
+        delete row._rowKey;
+        },
+        /**
+         *  修改行数据
+         *  @row 行数据
+         *  @currentPage 当前页
+         *  @pageSize 页大小
+         *  @force 强制更改元数据，刷新表格
+         */
+        alertRowData(row, currentPage, pageSize, force) {
+        let rowCount = (currentPage - 1) * pageSize;
+        let index = rowCount + row._index;
+        if (index >= 0 && !(this.objid > 0 && (this.basicData.status === '2' || this.basicData.status ==='3'))) {
+            this.giftData.gift_commoditylist[index].SUM_QTY = this.giftData.gift_commoditylist[index].SUM  = row.SUM
+            this.commodityList.data[row._index].SUM_QTY = row.SUM
+        }
+        },
+    }
+ }
