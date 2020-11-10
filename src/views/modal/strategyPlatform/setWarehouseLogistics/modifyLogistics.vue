2d79cc359af992092e591cb78234ff7a9dc7a9ab
xuhui <xu.h@burgeon.cn>
2020-11-10 11:23:03 +0800

    modifyLogistics.vue css,js引用路径修改

--- src/views/modal/strategyPlatform/setWarehouseLogistics/modifyLogistics.vue
+++ src/views/modal/strategyPlatform/setWarehouseLogistics/modifyLogistics.vue
@@ -83,12 +83,12 @@
 </template>
 
 <script>
-  
-  import modifyLogistics from '@/js/pages/strategyPlatform/setWarehouseLogistics/modifyLogistics';
+
+  import modifyLogistics from '@/js/modal/strategyPlatform/setWarehouseLogistics/modifyLogistics';
 
   export default modifyLogistics;
 </script>
 
 <style lang="less">
-  @import "~@/css/pages/strategyPlatform/setWarehouseLogistics/modifyLogistics.less";
+  @import "~@/css/modal/strategyPlatform/setWarehouseLogistics/modifyLogistics.less";
 </style>
