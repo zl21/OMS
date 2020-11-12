bcc5c44881f3efd5f2fa1ad1be2f9fa8109dcd09
sunny <sun.qian@burgeon.cn>
2020-11-12 20:11:21 +0800

    出库通知单 按查询条件重传WMS按钮功能已经该功能按钮防暴力点击

--- src/service/modules/inventoryCenter.js
+++ src/service/modules/inventoryCenter.js
@@ -1,2 +1,8 @@
 // 库存中心
-export default {};
+import R3 from '@syman/burgeon-r3';
+
+const { network } = R3;
+export default {
+    sgPhyOutNoticesSendWMSAgainCondition: params => network.post('/p/cs/sgPhyOutNoticesSendWMSAgainCondition', params)
+
+};
---
+++ src/views/modal/InventoryCenter/queryConditionRetransmissionWms.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="queryConditionRetransmissionWms">
+    <!-- 出库通知单->按查询条件重传wms按钮 -->
+    <p>确定根据查询条件重传WMS?</p>
+    <div class="dialog-footer">
+      <Button
+        type="primary"
+        size="small"
+        @click="determine"
+      >
+        {{ vmI18n.t("common.determine") }}
+      </Button>
+      <!-- 取消 -->
+      <Button
+        type="error"
+        ghost
+        size="small"
+        @click="
+          () => {
+            this.$emit('closeActionDialog');
+          }
+        "
+      >
+        {{ vmI18n.t("common.cancel") }}
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        vmI18n: window.vmI18n,
+        is_click: false,
+      };
+    },
+    computed: {
+      allFormData() {
+        return this.$store.state[`S.${this.$route.params.tableName}.${this.$route.params.tableId}`].formItems.data;
+      },
+      user() {
+        return this.$store.state.global.userInfo;
+      }
+    },
+    methods: {
+      determine() {
+        if (this.is_click) {
+          return false;
+        }
+        if (this.allFormData.CP_C_PHY_WAREHOUSE_ID || (this.allFormData.BILL_DATE[0] && this.allFormData.BILL_DATE[1]) || this.allFormData.SOURCE_BILL_NO) {
+          const obj = {};
+          obj.sourceBillType = this.allFormData.SOURCE_BILL_NO;
+          obj.phyWarehouseIds = this.allFormData.CP_C_PHY_WAREHOUSE_ID;
+          obj.startBillDate = this.allFormData.BILL_DATE[0];
+          obj.endBillDate = this.allFormData.BILL_DATE[1];
+          obj.user = this.user;
+          this.is_click = true;
+          setTimeout(()=>{
+            this.is_click = false;
+          }, 180000);
+          this.service.inventoryCenter.sgPhyOutNoticesSendWMSAgainCondition(obj).then(res=>{
+            console.log(res);
+            if (res.data.code == 0) {
+              this.$Message.success(res.data.data);
+            } else {
+              this.is_click = false;
+              this.$Message.error(res.data.data);
+            }
+          });
+        } else {
+          this.$Message.warning('查询条件 <实体仓> <单据日期> <来源单据类型> 至少有一个不为空.');
+        }
+      }
+    },
+    mounted() {
+
+    }
+  };
+</script>
+实体仓:"CP_C_PHY_WAREHOUSE_ID"
+单据日期:"BILL_DATE"
+来源单据类型:"SOURCE_BILL_NO"
+传WMS状态:"WMS_STATUS"
