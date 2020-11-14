cd7ffdb53fa69a911cd69decbb4af67e3b373aa8
sunny <sun.qian@burgeon.cn>
2020-11-14 16:10:57 +0800

    库存同步队列表 库存缓存池计算页面迁移

--- src/config/module/page/inventory.config.js
+++ src/config/module/page/inventory.config.js
@@ -1,10 +1,18 @@
 
 // 库存中心定制界面
 import outboundPickOrder from '@/views/pages/OrdersCenter/scanIn/outboundPickOrder.vue';
+import sgChannelSynstockQuery from '@/views/pages/InventoryCenter/sgChannelSynstockQuery.vue'; // 库存同步队列表
+import sgChannelStorageBuffer from '@/views/pages/InventoryCenter/sgChannelStorageBuffer.vue'; // 库存计算缓存池
 
 export default {
   // 出库通知单==>生成出库拣货单==>出库拣货单
   OUTBOUNDPICKORDER: {
     component: outboundPickOrder,
   },
+  SGCHANNELSYNSTOCKQUERY: {
+    component: sgChannelSynstockQuery
+  },
+  SGCHANNELSTORAGEBUFFER: {
+    component: sgChannelStorageBuffer
+  }
 };
