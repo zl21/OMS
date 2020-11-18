3384da22b57821c926afdcb6a5045f2e0843b70f
sunny <sun.qian@burgeon.cn>
2020-11-18 14:18:04 +0800

    库存调整单

--- src/views/modal/InventoryCenter/queryConditionRetransmissionWms.vue
+++ src/views/modal/InventoryCenter/queryConditionRetransmissionWms.vue
@@ -48,10 +48,10 @@
         if (this.is_click) {
           return;
         }
-        if (this.allFormData.CP_C_PHY_WAREHOUSE_ID || (this.allFormData.BILL_DATE[0] && this.allFormData.BILL_DATE[1]) || this.allFormData.SOURCE_BILL_NO) {
+        if (this.allFormData.CP_C_PHY_WAREHOUSE_ID || (this.allFormData.BILL_DATE[0] && this.allFormData.BILL_DATE[1]) || this.allFormData.SOURCE_BILL_TYPE) {
           const formdata = new FormData();
           const obj = {};
-          obj.sourceBillType = this.allFormData.SOURCE_BILL_NO;
+          obj.sourceBillType = this.allFormData.SOURCE_BILL_TYPE;
           obj.phyWarehouseIds = this.allFormData.CP_C_PHY_WAREHOUSE_ID;
           obj.startBillDate = this.allFormData.BILL_DATE[0];
           obj.endBillDate = this.allFormData.BILL_DATE[1];
@@ -60,11 +60,12 @@
           this.is_click = true;
           this.service.inventoryCenter.sgPhyOutNoticesSendWMSAgainCondition(formdata).then(res=>{
             console.log(res);
-            if (res.code == 0) {
-              this.$Message.success(res.data);
+            if (res.data.code == 0) {
+              this.$Message.success(res.data.message);
+              this.$emit('closeActionDialog');
             } else {
               this.is_click = false;
-              this.$Message.error(res.data);
+              this.$Message.error(res.data.message);
             }
           });
         } else {
