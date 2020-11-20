e9e52dc8a6de1b28ce150613e57c009434700ef4
sunny <sun.qian@burgeon.cn>
2020-11-20 10:24:49 +0800

    修改重传wms字段功能需求完成

--- src/views/modal/orderCenter/modifyWms.vue
+++ src/views/modal/orderCenter/modifyWms.vue
@@ -2,7 +2,7 @@
   <div class="modifyWms">
     <div class="form">
       <label>修改传WMS拣货单字段 : </label>
-      <Input v-model="sticsNumber" />
+      <Input v-model="send_wms_pick" />
     </div>
     <div class="dialog-footer">
       <!-- 确定 -->
@@ -35,7 +35,7 @@
     name: 'ModifyWms',
     data() {
       return {
-        sticsNumber: '',
+        send_wms_pick: '',
         vmI18n: window.vmI18n
       };
     },
@@ -48,25 +48,24 @@
     methods: {
       determine() {
         const self = this;
-        alert('暂无服务');
-        // if (self.sticsNumber) {
-        //   const obj = {};
-        //   obj.ids = self.idArray;
-        //   obj.logisticNumber = self.sticsNumber;
-        //   const formdata = new FormData();
-        //   formdata.append('param', JSON.stringify(obj));
-        //   self.service.orderCenter.updateLogisticsBeforePacking(formdata).then(res=>{
-        //     console.log(res);
-        //     if (res.data.code == 0) {
-        //       self.$Message.success(res.data.message);
-        //       self.$emit('closeActionDialog');
-        //     } else {
-        //       self.$Message.error(res.data.message);
-        //     }
-        //   });
-        // } else {
-        //   self.$Message.warning('请输入传WMS拣货单字段!');
-        // }
+        if (self.send_wms_pick) {
+          const obj = {};
+          obj.ids = self.idArray;
+          obj.send_wms_pick = self.send_wms_pick;
+          const formdata = new FormData();
+          formdata.append('param', JSON.stringify(obj));
+          self.service.orderCenter.sendWmsPick(formdata).then(res=>{
+            console.log(res);
+            if (res.data.data.code == 0) {
+              self.$Message.success(res.data.data.message);
+              self.$emit('closeActionDialog');
+            } else {
+              self.$Message.error(res.data.data.message);
+            }
+          });
+        } else {
+          self.$Message.warning('请输入传WMS拣货单字段!');
+        }
       }
     }
   };
