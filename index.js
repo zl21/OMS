843ef701fdde9ea00e2f697d190183a245a55cf5
段若愚 <duan.ry@burgeon.cn>
2020-12-08 16:41:12 +0800

    1.  调整框架包体配置, 并添加包体配置文件;
    2.  更新框架依赖包版本;

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// import arkUI from '@syman/ark-ui';
 import R3 from '@syman/burgeon-r3';
 
 // 企业Logo、Banner配置信息
 import apiPath from '@/assets/js/api/path/index';
 import i18n from '@burgeon/internationalization/i18n/i18n'; // 国际化
 import service from '@/service/index';
+
 import qs from 'qs';
 import request, { httpFormdata } from 'framework/__utils__/request';
 import ajax from 'framework/__utils__/ajax';
@@ -13,25 +13,22 @@ import groups from '@/assets/js/promotion/groups';
 import comUtils from '@/assets/js/__utils__/common';
 import enterpriseLogo from './src/assets/image/logo.png';
 import enterpriseBanner from './src/assets/image/banner.png';
-import Login from './src/component/Login.vue';
-import WelcomePage from './src/component/WelcomePage.vue';
+import Login from './src/component/Login';
+import WelcomePage from './src/component/WelcomePage';
 import projectRouterConfig from 'burgeonConfig/router.config';
 import customizedPageConfig from 'burgeonConfig/customized.page.config';
 import customizedModalConfig from 'burgeonConfig/customized.modal.config';
 import customizeWaterMarkConfig from 'burgeonConfig/customized.watermark.config';
 import filterUrlConfig from 'burgeonConfig/config/filterUrl.config';
+import myfilterUrlForNetworkScript from '@/service/filterUrlForNetwork';
 // ----------动态加载主题库皮肤包;
 
 import '@burgeon/oms-theme/theme/custom.less'; // 主题文件
 
 import externalTreeDatasConfig from './src/config/externalTreeDatas.config'; // 树结构配置
-// import './static/theme/custom.less'; // 主题文件
-// import './static/theme/theme.less'; // 自定义主题文件
-// import '@syman/ark-ui/dist/styles/ark-ui.css';
 import '@syman/burgeon-r3/r3.publish/r3.min.css';
 
 // 老框架引入的 自定义界面内需要的css资源
-// import 'element-ui/lib/theme-chalk/index.css'; // customize-1.3
 import './static/iconfont/iconfont.css'; // customize-1.3自定义界面icon,下拉单选,此样式会影响框架样式，
 import './static/promotion.icofont/iconfont.css'; // 促销iconfont
 import '@/assets/css/css_1_3/index.less'; // customize-1.3自定义界面icon,下拉单选
@@ -44,6 +41,8 @@ import connector from './src/js/pages/common/orderDetail/connector';
 
 // -------------引入框架项目配置文件;
 const customizedTheme = require(`@burgeon/oms-theme/skin/${omsSkinTheme}/index.min.css`).default;
+filterUrlConfig.customizePage = filterUrlConfig.customizePage.flat();
+console.log(filterUrlConfig);
 groups.load();
 Vue.prototype.$ajax = ajax;
 Vue.prototype.axios = request;
@@ -83,5 +82,6 @@ R3.launchApplication({
   projectRoutes: projectRouterConfig, // 项目自定义路由，一般情况下用不到此配置
   // quietRoutes: ['/register', '/password', '/forgotPassword'], // 外置路由
   externalTreeDatas: externalTreeDatasConfig,
-  filterUrlForNetwork: filterUrlConfig // 过滤不需要用框架报错提示信息的接口请求
+  filterUrlForNetwork: filterUrlConfig, // 过滤不需要用框架报错提示信息的接口请求
+  filterUrlForNetworkScript: myfilterUrlForNetworkScript // 导入的filterUrlForNetworkScript脚本文件
 });
