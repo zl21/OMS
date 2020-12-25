098036db7ce829bb3886878b6e99a008c9a25a86
xuhui <xu.h@burgeon.cn>
2020-12-25 15:46:32 +0800

    撤回上个提交把downLoadAll误提交

--- src/views/modal/InterfacePlatform/downLoadAll.vue
+++ src/views/modal/InterfacePlatform/downLoadAll.vue
@@ -1,35 +1,40 @@
 <template>
-  <div style="width:440px;" />
+  <div
+    class="downLoadTaobaoOrder"
+    style="width:430px"
+  >
+    <businessForm :form-config="downLoadFormConfig" />
+    <businessButton :btn-config="downLoadBtnConfig" />
+    <!-- 确认下载弹框 订单下载-->
+    <Modal
+      v-model="downLoadModal"
+      class="downLoadModal"
+      :title="vmI18n.t('modalTitle.orderDownload')"
+      width="450"
+      :mask="true"
+      @on-ok="downLoadOk"
+      @on-cancel="downLoadCancel"
+    >
+      <p>
+        <!-- 订单下载任务已经发送，任务ID： -->
+        {{ vmI18n.t("modalTips.bn") }}
+        <span
+          class="taskID"
+          @click="taskIDClick"
+        >{{ taskId }}</span>
+        <!-- ，请前往接口下载任务表查看下载进度！ -->
+        {{ vmI18n.t("modalTips.bo") }}
+      </p>
+    </Modal>
+  </div>
 </template>
 
 <script>
-  export default {
-    components: {},
-    props: {
-      idArray: {
-        type: Array,
-        default: () => []
-      },
-      objList: {
-        type: Array
-      },
-    },
-    data() {
-      return {};
-    },
-    methods: {
-      init() {
-        if (this.props.idArray.length !== 1) {
-          this.$message.error('请先选择一条数据更换吊牌！');
-        }
-        // if () {
-        //   this.$message.error('当前单据状态不可操作!')
-        // }
-      }
-    },
-    mounted() {
-      // this.$emit('closeActionDialog');
-      this.init();
-    }
-  };
+  import downLoadAll from '@/js/modal/InterfacePlatform/downLoadAll';
+
+  export default downLoadAll;
 </script>
+
+<style >
+  @import "~@/css/modal/InterfacePlatform/downLoadAll.less";
+</style>
