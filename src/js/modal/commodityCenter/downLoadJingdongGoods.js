49c8836315eccabc8955e01b81b965cb05abd2f5
sunny <sun.qian@burgeon.cn>
2021-01-17 00:01:49 +0800

    产线bug: 31989【京东商品】按照商品id下载提示“商品id和时间必选择一个”

--- src/js/modal/commodityCenter/downLoadJingdongGoods.js
+++ src/js/modal/commodityCenter/downLoadJingdongGoods.js
@@ -57,7 +57,7 @@ export default {
           {
             style: 'input', // 输入框类型
             label: window.vmI18n.t('form_label.commodityID'), // 商品ID 输入框前文字
-            value: vmI18n.t('form_label.commodityID'), // 输入框的值
+            value: 'shopID', // 输入框的值
             width: '24', // 所占的宽度 (宽度分为24份,数值代表所占份数的宽度)
             icon: '', // 输入框后带的图标,暂只有输入框支持
             placeholder: '', // 占位文本，默认为请输入
