9b2335f1c2c2e5257e0d8388b48802102997eaa0
sunny <sun.qian@burgeon.cn>
2021-03-04 10:38:32 +0800

    额外退款详情跳转问题处理

--- src/js/pages/orderCenter/returngood/refundAfterShipment.js
+++ src/js/pages/orderCenter/returngood/refundAfterShipment.js
@@ -852,8 +852,7 @@ export default {
     this.handleAuditBtnDisplay();
     const customizeMessage = sessionStorage.getItem('customizeMessage');
     if (customizeMessage) {
-      // this.sessionStorageData = JSON.parse(customizeMessage)[this.$route.params.customizedModuleId == '41460334' ? 'undefined' : this.$route.params.customizedModuleId];
-      this.sessionStorageData = JSON.parse(customizeMessage).EXTRAREFUND;
+      this.sessionStorageData = JSON.parse(customizeMessage)[this.$route.params.customizedModuleId == '41460334' ? 'undefined' : this.$route.params.customizedModuleId];
     }
     // if (this.$route.query.id && !this.$route.query.new) {
     // eslint-disable-next-line no-mixed-operators
