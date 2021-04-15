98d8151cf201ad32ee16a8af586689f530e28929
珠宝-熊伟 <879741443@qq.com>
2021-04-15 17:31:29 +0800

    !84 33598 * 平台退款编号文案修改

--- src/js/modal/InterfacePlatform/config/IP_B_TAOBAO_REFUND.js
+++ src/js/modal/InterfacePlatform/config/IP_B_TAOBAO_REFUND.js
@@ -65,7 +65,7 @@ export default {
       },
       {
         style: 'input', // 输入框类型
-        label: window.vmI18n.t('form_label.platform_returnOrder_no'), // 平台退货单号 输入框前文字
+        label: '平台退款编号', // 平台退货单号 输入框前文字
         value: 'bill_no', // 输入框的值
         width: '24', // 所占的宽度 (宽度分为24份,数值代表所占份数的宽度)
         icon: '', // 输入框后带的图标,暂只有输入框支持
