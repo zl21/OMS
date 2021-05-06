21864200ed8ced2a15e253ac19378fb2b9a83846
zhou.l <zhou.l@burgeon.cn>
2021-05-06 14:31:12 +0800

    动态路由

--- config/module/page/basicData.config.js
+++ config/module/page/basicData.config.js
@@ -1,34 +1,40 @@
 /* 2.0: */
-import logisticsCompanyFilesAddOrEdit from 'allpages/basicData/logisticsCompanyFilesAddOrEdit.vue'; // 物流公司档案-新增/详情
-import addLogicStore from 'allpages/basicData/addLogicStore.vue'; // 逻辑仓新增
-import addAliasOrRegion from 'allpages/basicData/addAliasOrRegion.vue'; // 国家省市区-新增别名/新增区域
-import nationalProvincialMunicipalEdit from 'allpages/basicData/nationalProvincialMunicipalEdit.vue'; // 国家省市区-详情
-import channelStore from 'allpages/basicData/channelStore.vue'; // 渠道仓新增
+// import logisticsCompanyFilesAddOrEdit from 'allpages/basicData/logisticsCompanyFilesAddOrEdit.vue'; // 物流公司档案-新增/详情
+// import addLogicStore from 'allpages/basicData/addLogicStore.vue'; // 逻辑仓新增
+// import addAliasOrRegion from 'allpages/basicData/addAliasOrRegion.vue'; // 国家省市区-新增别名/新增区域
+// import nationalProvincialMunicipalEdit from 'allpages/basicData/nationalProvincialMunicipalEdit.vue'; // 国家省市区-详情
+// import channelStore from 'allpages/basicData/channelStore.vue'; // 渠道仓新增
 
 export default {
   /* 2.0: */
   LOGISTICSCOMPANYFILESADDOREDIT: {
-    component: logisticsCompanyFilesAddOrEdit,
-    labelName: '物流公司档案编辑',
+    // component: logisticsCompanyFilesAddOrEdit,
+    component: () => import('allpages/basicData/logisticsCompanyFilesAddOrEdit.vue'),
+    // labelName: '物流公司档案编辑',
   },
   ADDLOGICSTORE: {
-    component: addLogicStore,
-    labelName: '逻辑仓档案新增'
+    // component: addLogicStore,
+    component: () => import('allpages/basicData/addLogicStore.vue'),
+    // labelName: '逻辑仓档案新增'
   },
   ADDALIAS: {
-    component: addAliasOrRegion,
-    labelName: '新增别名'
+    // component: addAliasOrRegion,
+    component: () => import('allpages/basicData/addAliasOrRegion.vue'),
+    // labelName: '新增别名'
   },
   ADDREGION: {
-    component: addAliasOrRegion,
-    labelName: '新增区域'
+    // component: addAliasOrRegion,
+    component: () => import('allpages/basicData/addAliasOrRegion.vue'),
+    // labelName: '新增区域'
   },
   CHANNELSTORE: {
-    component: channelStore,
-    labelName: '渠道仓新增'
+    // component: channelStore,
+    component: () => import('allpages/basicData/channelStore.vue'),
+    // labelName: '渠道仓新增'
   },
   NATIONALPROVINCIALMUNICIPALEDIT: {
-    component: nationalProvincialMunicipalEdit,
-    labelName: '国家省市区编辑'
+    // component: nationalProvincialMunicipalEdit,
+    component: () => import('allpages/basicData/nationalProvincialMunicipalEdit.vue'),
+    // labelName: '国家省市区编辑'
   },
-}; 
+};
--- config/module/page/finance.config.js
+++ config/module/page/finance.config.js
@@ -1,15 +1,17 @@
 // 财务中心-赔付单
-import payableAdjustmentList from 'allpages/financialCenter/payableAdjust/payableAdjustmentList';
+// import payableAdjustmentList from 'allpages/financialCenter/payableAdjust/payableAdjustmentList';
 // 财务中心-赔付单新增
-import payableAdjustAdd from 'allpages/financialCenter/payableAdjust/payableAdjustAdd';
+// import payableAdjustAdd from 'allpages/financialCenter/payableAdjust/payableAdjustAdd';
 
 export default {
   // 赔付单
   PAYABLEADJUSTMENTLIST: {
-    component: payableAdjustmentList,
+    component: () => import('allpages/financialCenter/payableAdjust/payableAdjustmentList'),
+    // component: payableAdjustmentList,
   },
   // 新增赔付单
   PAYABLEADJUSTADD: {
-    component: payableAdjustAdd,
+    component: () => import('allpages/financialCenter/payableAdjust/payableAdjustAdd'),
+    // component: payableAdjustAdd,
   },
 };
