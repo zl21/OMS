55043c1612f2e51c88a6705dc611c52d9611456c
sunny <sun.qian@burgeon.cn>
2021-05-26 19:48:26 +0800

    新增退货确认策略定制面板->执行动作

--- config/config/customized.panel.config.js
+++ config/config/customized.panel.config.js
@@ -1,15 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2021-05-19 13:11:45
- * @LastEditTime: 2021-05-21 18:15:40
+ * @LastEditTime: 2021-05-26 19:45:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /project-logic/config/module/page/coustomPage.config.js
  */
 import returnGoods from '@/views/pages/orderCenter/returngood/returnGoods.vue';
 import returnAmount from '@/views/pages/orderCenter/returngood/returnAmount.vue';
+import remark from '@/views/pages/strategyPlatform/preformAction.vue'
 let custommizeMain = {
   VERSION: returnGoods,
-  PRO_ACTUAL_AMT:returnAmount
+  PRO_ACTUAL_AMT:returnAmount,
+  REMARK: remark
 }
 export default custommizeMain;
---
+++ views/pages/strategyPlatform/preformAction.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-05-26 19:42:35
+ * @LastEditTime: 2021-05-26 19:46:40
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-project-logic/views/pages/strategyPlatform/preformAction.vue
+-->
+<template>
+    <div class="preformAction">
+        <i>*</i>&nbsp;<span>自动确认退货(仓库回传退货入库结果后，系统自动完成退货确认的业务操作)</span>
+    </div>
+</template>
+<style scoped lang="less">
+.preformAction {
+    padding-left: 10px;
+    i {
+        color: red;
+    }
+}
+</style>
