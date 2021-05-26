55aeea02143710e2cdacd56b03fd7ffbddd9c032
xw <xiongwei@xiongweidembp>
2021-05-26 14:19:12 +0800

    sku异常登记

---
+++ src/js/pages/orderCenter/returngood/skuAbnormalRegistration.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import jordanBtn from 'professionalComponents/businessButton';
+
+export default {
+  components: {
+    jordanBtn
+  },
+  data() {
+    return {
+        
+    };
+  },
+  methods: {
+      
+  },
+  mounted() {
+      
+  }
+};
---
+++ src/views/pages/OrderCenter/returngood/skuAbnormalRegistration.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bounced">
+      111
+  </div>
+</template>
+
+<script>
+  import skuAbnormalRegistration from '@/js/pages/orderCenter/returngood/skuAbnormalRegistration';
+
+  export default skuAbnormalRegistration;
+</script>
+
+<style lang="less">
+
+</style>
