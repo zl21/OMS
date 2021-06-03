d25cbace30293a7f4635fe3d1f411b323fa38d8f
sunny <sun.qian@burgeon.cn>
2021-06-03 11:01:44 +0800

    vue-ag新建测试

---
+++ businessAgTable.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-06-02 11:33:11
+ * @LastEditTime: 2021-06-02 11:40:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-business-components/businessAgTable.vue
+-->
+<template>
+    <div class="businessAgTable">
+        <!-- <commonTableByAgGrid></commonTableByAgGrid> -->
+        123
+    </div>
+</template>
+
+<script>
+import businessAgTable from './js/businessAgTable'
+export default businessAgTable
+</script>
---
+++ js/businessAgTable.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-02 11:34:07
+ * @LastEditTime: 2021-06-02 11:38:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-business-components/js/businessAgTable.js
+ */
+import commonTableByAgGrid from 'libs/@syman/ark-ui-bcl/src/components/common-table-by-ag-grid/CommonTableByAgGrid'
+export default {
+    components:{
+        commonTableByAgGrid
+    },
+}
