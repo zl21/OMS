86e706252e9707324d7fb8f4cd5053c280650f32
吴凯 <wu.k@burgeon.cn>
2021-06-08 20:32:24 +0800

    增加功能权限和数据权限定制页面

---
+++ config/config/functionPower.actions.js
@@ -0,0 +1,83 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-08 14:17:52
+ * @LastEditTime: 2021-06-08 15:38:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-project-logic/config/config/functionPower.actions.js
+ */
+/* eslint-disable import/prefer-default-export */
+import R3 from '@syman/burgeon-r3';
+
+const { network } = R3;
+
+
+export const fetchActionsInCustomizePage = function fetchActionsInCustomizePage(params) {
+  network
+    .post('/r3-cp/p/cs/fetchActionsInCustomizePage', params.params)
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
+
+export const queryMenuPermission = function queryMenuPermission(params) {
+  network
+    .post('/r3-cp/p/cs/queryMenuPermission', params.params)
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
+
+export const copyPermission = function copyPermission(params) {
+  network
+    .post('/p/cs/copyPermission', params.params)
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
+
+export const savePermission = function savePermission(params) {
+  network
+    .post('/p/cs/savePermission', params.params)
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
+
+export const cgroupsquery = function cgroupsquery(params) {
+  network
+    .post('/p/cs/cgroupsquery', params.params)
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
+
+export const getMenuTree = function getMenuTree(params) {
+  network
+    .post('/r3-cp/p/cs/getMenuTree')
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
+
+export const groupTreeload = function groupTreeload(params) {
+  network
+    .post('/r3-cp/p/cs/groupTreeload')
+    .then((res) => {
+      if (typeof params.success === 'function') {
+        params.success(res);
+      }
+    });
+};
--- views/pages/strategyPlatform/specialLogistics.vue
+++ views/pages/strategyPlatform/specialLogistics.vue
@@ -1,13 +1,16 @@
 <!--
  * @Author: your name
  * @Date: 2021-05-18 17:02:53
- * @LastEditTime: 2021-06-03 10:45:29
+ * @LastEditTime: 2021-06-08 10:02:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /云雀/src/views/pages/strategyPlatform/specialLogistics.vue
 -->
 <template>
-  <div class="specialLogistics otherClass customized-detail" v-loading="loading">
+  <div
+    class="specialLogistics otherClass customized-detail"
+    v-loading="loading"
+  >
     <!-- <loading :loading="loading" /> -->
     <div class="buttons customized-detail-btn">
       <businessButton :btn-config="btnConfig" />
@@ -17,7 +20,11 @@
         <Panel name="panel_baseInfo">
           基本信息
           <div slot="content">
-            <businessForm :form-config="formConfighead" @keyDown="keyDown" :key="force" />
+            <businessForm
+              :form-config="formConfighead"
+              @keyDown="keyDown"
+              :key="force"
+            />
           </div>
         </Panel>
       </Collapse>
@@ -26,12 +33,23 @@
         <Panel name="panel_baseInfo1">
           满足条件
           <div slot="content">
-            <businessForm :form-config="formConfig" @keyDown="keyDown" :key="force" />
+            <businessForm
+              :form-config="formConfig"
+              @keyDown="keyDown"
+              :key="force"
+            />
             <div class="content-li">
-              <Button type="info" class="content-li-btn" @click="fnselect">请选择省市区</Button>
+              <Button type="info" class="content-li-btn" @click="fnselect"
+                >请选择省市区</Button
+              >
             </div>
             <div class="content-li">
-              <Table width="550" border :columns="columnsCity" :data="dataCity"></Table>
+              <Table
+                width="550"
+                border
+                :columns="columnsCity"
+                :data="dataCity"
+              ></Table>
             </div>
 
             <switchList :switchList="switchListdata"></switchList>
@@ -41,7 +59,12 @@
               <div class="content-li-item-left">
                 <Button type="info" @click="fnseek">添加商品</Button>
                 <div class="content-li-item-left-a">
-                  <Table width="550" border :columns="columns2" :data="data3"></Table>
+                  <Table
+                    width="550"
+                    border
+                    :columns="columns2"
+                    :data="data3"
+                  ></Table>
                 </div>
               </div>
             </div>
@@ -49,49 +72,75 @@
         </Panel>
       </Collapse>
 
-       <Collapse v-model="panelDefaultValue2">
+      <!-- <Collapse v-model="panelDefaultValue2">
         
         <Panel name="panel_baseInfo2" >
-           <businessLabel
-            :label-list="labelList"
-            :label-default-value="labelDefaultValue"
-            @labelClick="labelClick"
-          />
-          <div slot="content" v-show="labelDefaultValue == 'PROPERTY'">
-            <businessForm :form-config="formConfig1" @keyDown="keyDown" />
-            <businessButton :btn-config="btnConfig1" />
-            <Table border ref="selection" :columns="columns4" :data="data1" @on-select="data1select"></Table>
-            <Page :total="total2" show-sizer class-name="tablestyle" @on-change="tablepage2" @on-page-size-change="tablesize2" />
-          </div>
+          
         </Panel>
 
           <div class="subtablePart" v-show="labelDefaultValue != 'PROPERTY'">
           <subTable :component-data="subTableConfig"></subTable>
           </div>
-      </Collapse>
-
-
+      </Collapse> -->
     </div>
 
-
-    <!-- <div class="customized-detail-table">
-      <div class="subtablePart">
-       
-       </div>
-    </div> -->
+    <div class="customized-detail-table">
+      <businessLabel
+        :label-list="labelList"
+        :label-default-value="labelDefaultValue"
+        @labelClick="labelClick"
+      />
+      <div slot="content" v-show="labelDefaultValue == 'PROPERTY'">
+        <businessForm :form-config="formConfig1" @keyDown="keyDown" />
+        <businessButton :btn-config="btnConfig1" />
+        <Table
+          border
+          ref="selection"
+          :columns="columns4"
+          :data="data1"
+          @on-select="data1select"
+        ></Table>
+        <Page
+          :total="total2"
+          show-sizer
+          class-name="tablestyle"
+          @on-change="tablepage2"
+          @on-page-size-change="tablesize2"
+        />
+      </div>
+      <div class="subtablePart" v-show="labelDefaultValue != 'PROPERTY'">
+        <subTable :component-data="subTableConfig"></subTable>
+      </div>
+    </div>
 
     <!-- 弹窗组件 -->
-    <Modal v-model="modal2" width="460" mask @on-ok="fnok" @on-cancel="fncancelcity">
+    <Modal
+      v-model="modal2"
+      width="460"
+      mask
+      @on-ok="fnok"
+      @on-cancel="fncancelcity"
+    >
       <businessForm :form-config="formConfig2" @keyDown="keyDown" />
     </Modal>
     <Modal v-model="modal3" width="660" mask footer-hide>
       <div class="specialLogistics-model-head">
         <div class="specialLogistics-model-head-left">
-          <Input placeholder="SPU编码/SKU编码" style="width: auto" @on-enter="fnkeyup"  @on-click="fnkeyup"
-           icon="ios-search" >
+          <Input
+            placeholder="SPU编码/SKU编码"
+            style="width: auto"
+            @on-enter="fnkeyup"
+            @on-click="fnkeyup"
+            icon="ios-search"
+          >
           </Input>
-          <Input placeholder="SPU名称/SKU名称" style="width: auto" @on-enter="fnkeyup1" @on-click="fnkeyup1"
-          icon="ios-search">
+          <Input
+            placeholder="SPU名称/SKU名称"
+            style="width: auto"
+            @on-enter="fnkeyup1"
+            @on-click="fnkeyup1"
+            icon="ios-search"
+          >
           </Input>
         </div>
         <div class="specialLogistics-model-head-right">
@@ -99,7 +148,15 @@
           <Button type="error" ghost @click="fncancel">取消</Button>
         </div>
       </div>
-      <businessActionTable :jordan-table-config="tableConfig" @on-page-change="pageChange" @on-page-size-change="pageSizeChange" @on-select="onSelect" @on-select-cancel="onSelectCancel" @on-select-all="onSelectAll" @on-select-all-cancel="onSelectAllCancel" />
+      <businessActionTable
+        :jordan-table-config="tableConfig"
+        @on-page-change="pageChange"
+        @on-page-size-change="pageSizeChange"
+        @on-select="onSelect"
+        @on-select-cancel="onSelectCancel"
+        @on-select-all="onSelectAll"
+        @on-select-all-cancel="onSelectAllCancel"
+      />
     </Modal>
   </div>
 </template>
@@ -111,5 +168,5 @@ export default logisticsNoun;
 </script>
 
 <style lang="less" scoped>
-@import '~@/css/pages/strategyPlatform/specialLogistics.less';
+@import "~@/css/pages/strategyPlatform/specialLogistics.less";
 </style>
