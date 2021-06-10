a42f4f824853e6f0a1cc9d9cecd08b956fd065f8
liushuanglian <li.sl@burgeon.cn>
2021-06-10 14:50:05 +0800

    v1.5.62 | 通用退单下载

--- js/modal/interfacePlatform/config/IP_B_JINGDONG_REFUND.js
+++ js/modal/interfacePlatform/config/IP_B_JINGDONG_REFUND.js
@@ -3,84 +3,91 @@ export default {
   // 京东退单接口列表界面(下载退单)
   formConfig: {
     formValue: {
-      numNumber: ''
+      startEndTimes: [],
+      bill_no: '',
+      service_no: ''
     },
     formData: [
       {
         style: 'popInput', // 输入框弹框单多选
         width: '24',
+        version: '1.4',
+        inputList: [],
         isActive: true,
         isdisabled: false,
-        inputList: [
-          {
-            childs: [{ colname: 'CP_C_SHOP_ID', refobjid: 4, valuedata: 2 }]
-          }
-        ],
         itemdata: {
-          col: 1,
-          colid: 167606,
+          colid: 181965,
           colname: 'CP_C_SHOP_ID', // 当前字段的名称
-          datelimit: 'all',
-          refcolval: {
-            fixcolumn: 'CP_C_PLATFORM_ID',
-            expre: 'equal',
-            srccol: 'CP_C_SHOP_ID'
-          },
-          display: 'text', // 显示什么类型，例如xml表示弹窗多选加导入功能，mrp表示下拉多选
           fkdisplay: 'drp', // 外键关联类型
-          fkdesc: $i18n.t('other.shop'), // 店铺 
-          inputname: 'CP_C_SHOP_ID', // 这个是做中文类型的模糊查询字段，例如ENAME
           isfk: true, // 是否有fk键
           isnotnull: true, // 是否必填
-          isuppercase: false, // 是否转大写
-          length: 65535, // 最大长度是多少
-          name: $i18n.t('other.shop'), // 店铺 input前面显示的lable值
+          name: '店铺', // 店铺 input前面显示的lable值
           readonly: false, // 是否可编辑，对应input   readonly属性
-          reftable: 'CP_C_SHOP',
-          reftableid: 24475,
-          row: 1,
-          statsize: -1,
-          type: 'STRING',
-          valuedata: '' // 这个是选择的值
-        }
+          pid: '',
+          valuedata: '', // 这个是选择的值
+        },
+        oneObj: () => {}
       },
       {
-        style: 'date',
-        type: 'datetimerange', // 日期组件类型,默认为data  (daterange)为双日期区间选择
-        value: 'query_date',
-        label: $i18n.t('form_label.platformTime'), // 平台时间
-        width: '24',
-        format: 'yyyy-MM-dd HH:mm:ss', // 格式参照burgeonui
-        placeholder: ''
+        style: 'date', // 输入框类型
+        label: '退单修改时间', // 输入框前文字
+        colname: 'startEndTimes',
+        type: 'daterange',
+        value: 'startEndTimes', // 输入框的值
+        width: '24', // 所占的宽度 (宽度分为24份,数值代表所占份数的宽度)
+        format: 'yyyy-MM-dd HH:mm:ss',
+        onChange: () => {}
+      },
+      {
+        style: 'input', // 输入框类型
+        label: '平台单号',
+        value: 'bill_no', // 输入框的值
+        width: '24', // 所占的宽度 (宽度分为24份,数值代表所占份数的宽度)
+        ghost: false, // 是否关闭幽灵按钮，默认开启
+        inputenter: () => {}, // 表单回车事件
+      },
+      {
+        style: 'input', // 输入框类型
+        label: '服务单号',
+        value: 'service_no', // 输入框的值
+        width: '24', // 所占的宽度 (宽度分为24份,数值代表所占份数的宽度)
+        ghost: false, // 是否关闭幽灵按钮，默认开启
+        inputenter: () => {}, // 表单回车事件
       }
     ],
-    // 表单非空提示
     ruleValidate: {
-      numNumber: [{ required: true, message: ' ', trigger: 'blur' }],
-      query_date: [{ required: true }]
+      CP_C_SHOP_ID: [{
+        required: true,
+        message: ' '
+      }]
     }
   },
   // 确定按钮
   determine: async (self) => {
     const formValue = self.downLoadFormConfig.formValue;
-    const shopId = self.downLoadFormConfig.formData[0].itemdata.pid;
-    if (!shopId || !formValue.query_date[0]) {
-      self.$message.error($i18n.t('modalTips.bt'));// 店铺和平台时间不能为空
-      return;
+    if (
+      !self.downLoadFormConfig.formData[0].itemdata.pid
+    ) {
+      self.$Message.warning($i18n.t('modalTips.be')); // 请选择需要下载的店铺
+      return false;
     }
-    self.dialogLoad = true;
-    const params = {
-      table: self.$route.params.tableName,
-      shop_id: shopId,
-      start_time: BurgeonDate.standardTimeConversiondateToStr(formValue.query_date[0]),
-      end_time: BurgeonDate.standardTimeConversiondateToStr(formValue.query_date[1])
+    const [start, end] = formValue.startEndTimes
+    if (!(formValue.bill_no || start)) {
+      self.$Message.warning('请输入退单修改时间或平台单号');
+      return false;
+    }
+    const param = {
+      shop_id: self.downLoadFormConfig.formData[0].itemdata.pid,
+      bill_no: formValue.bill_no, // 平台单号
+      service_no: formValue.service_no, // 服务单号
+      start_time: start ? BurgeonDate.standardTimeConversiondateToStr(start) : '',
+      end_time: end ? BurgeonDate.standardTimeConversiondateToStr(end) : '',
+      table: self.$route.params.tableName // 当前表名 必传
     };
-    const fromdata = new FormData();
-    fromdata.append('param', JSON.stringify(params));
-      
+    self.dialogLoad = true;
     try {
       // 请求退单下载订单接口
-      const { data: { code, message } } = await self.service.interfacePlatform.refundDownload(fromdata);
+      const { data: { code, message } } = await self.service.interfacePlatform.refundDownload(param);
       if (code === 0) {
         self.dialogLoad = false;
         self.$message.success(message);
