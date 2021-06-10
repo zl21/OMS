ec77caa17e9b8c41f47506946a0fcd270d5fbc70
吴凯 <wu.k@burgeon.cn>
2021-06-10 14:26:48 +0800

    修复数据权限页面优化

--- service/modules/userCenter.js
+++ service/modules/userCenter.js
@@ -1,12 +1,13 @@
 /*
  * @Author: zhou.l
  * @Date: 2021-06-01 14:31:15
- * @LastEditTime: 2021-06-08 14:18:58
+ * @LastEditTime: 2021-06-10 14:14:16
  * @LastEditors: Please set LastEditors
  * @Description: 用户中心
  * @FilePath: /burgeon-project-logic/service/modules/userCenter.js
  */
 
-export default {
+export default { //
+    groupsTree: params => $network.post('/p/cs/cp/v1/groups/tree', params), // 组织档案
     hrorgTree: params => $network.post('/p/cs/cp/v1/hrorg/tree', params), // 组织档案
 };
