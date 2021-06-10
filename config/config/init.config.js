02126ccf3a1cdff2487f004d6867a0926016fbab
段若愚 <duan.ry@burgeon.cn>
2021-06-10 14:18:18 +0800

    1. 挂载全局页面备注配置模块;

--- config/config/init.config.js
+++ config/config/init.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: flybird
  * @Date: 2021-06-04 13:20:21
- * @LastEditTime: 2021-06-07 14:41:06
+ * @LastEditTime: 2021-06-10 14:17:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /burgeon-project-logic/config/config/init.config.js
@@ -11,6 +11,7 @@ import '@/assets/css/css_1_3/oms_index.less'; // 定制公共界面样式
 import R3 from '@syman/burgeon-r3';
 import Vue from 'vue';
 import comUtils from '@/assets/js/__utils__/common';
+import pageNote from 'burgeonConfig/config/pageNote'
 // import store from 'burgeonConfig/store/store'; // 将老框架公共状态注册为customize模块
 import wangEditor from 'wangeditor';
 import qs from 'qs';
@@ -24,7 +25,8 @@ class InitAppConfig {
     const customizedTheme = require(`@burgeon/oms-theme/skin/${omsSkinTheme}/index.min.css`).default;
     // window.$store = store;
     Vue.prototype.qs = qs;
-    window.$store=store;
+    window.$store = store;
+    window.$pageNote = pageNote;
     window.R3 = R3; // 暴露R3为全局变量
     window.$i18n = i18n; // 挂载国际化
     window.wangEditor = wangEditor;
@@ -33,7 +35,6 @@ class InitAppConfig {
     Vue.prototype.$lodash = lodash;
     Vue.prototype.service = service;
 
-    console.log('window.vmI18n',window.vmI18n);
     // 路由守卫 去掉部分定制界面onresize方法
     R3.router.afterEach(to => {
       const tableNameArr = ['ORDERMANAGER', 'PAYABLEADJUSTMENTLIST', 'returngoodmanagementList', 'manualMatching', 'RETURNSTOREAGELIST', 'PROMACTIQUERYLIST', 'SETWAREHOUSELOGISTICS'];
---
+++ config/config/pageNote.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: flybird
+ * @Date: 2021-06-10 11:03:40
+ * @LastEditTime: 2021-06-10 13:43:18
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-project-logic/config/config/pageNote.js
+ * key(tableName):value(msg)
+ */
+export default {
+    'OC_B_REFUND_ORDER_EXTRA': `<div style="color:red;line-height: 1;">提示：新增“退货退款”的额外退款单可能因平台重新推单，导致多退，请谨慎操作！</div>`
+}
