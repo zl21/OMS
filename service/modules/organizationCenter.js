9dfe24936457e8216d28ccd7abe303f68b32d1c3
吴凯 <wu.k@burgeon.cn>
2021-06-17 15:26:24 +0800

    增加用户中心重置密码

--- service/modules/organizationCenter.js
+++ service/modules/organizationCenter.js
@@ -1,2 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-07 20:40:40
+ * @LastEditTime: 2021-06-17 15:15:21
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-project-logic/service/modules/organizationCenter.js
+ */
 // 组织中心
-export default {};
+export default {
+    usersSave: (params) =>
+    $network.post('/p/cs/cp/v1/users/save', params), //修改密码
+  
+};
