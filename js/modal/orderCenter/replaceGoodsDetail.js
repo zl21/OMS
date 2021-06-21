529594df360e424c60814f197a38098618605d8a
weixiaoxiao <1655654318@qq.com>
2021-06-21 17:18:39 +0800

    零售发货单-bug:41477

--- js/modal/orderCenter/replaceGoodsDetail.js
+++ js/modal/orderCenter/replaceGoodsDetail.js
@@ -46,7 +46,7 @@ export default {
         width: '', // 表格宽度
         height: '', // 表格高度
         border: true, // 是否显示纵向边框
-        total: 10, // 设置总条数
+        total: 0, // 设置总条数
         pageSizeOpts: [10, 20, 30], // 每页条数切换的配置
         pageSize: 10, // 每页条数
       },
