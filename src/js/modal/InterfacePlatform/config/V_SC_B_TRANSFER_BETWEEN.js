3462fbaa788f5f641f607ffd6c82179a95c2bcd1
xw <xiongwei@xiongweidembp>
2021-07-07 18:38:50 +0800

    调拨管理-仓间调拨

---
+++ src/js/modal/InterfacePlatform/config/V_SC_B_TRANSFER_BETWEEN.js
@@ -0,0 +1,10 @@
+export default {
+  // 仓间调拨-审核
+  formConfig: {
+   
+  },
+  // 确定按钮
+  determine: (self) => {
+    // console.log('仓间调拨-审核');
+  }
+};
---
+++ src/views/modal/InventoryCenter/transferaudit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    style="width:750px"
+    class="gf"
+  >
+    
+    <business-action-table
+      :jordan-table-config="jordanTableConfig"
+      @on-select="returnOnSelect"
+      @on-select-cancel="returnCancel"
+      @on-select-all="returnSelectAll"
+      @on-select-all-cancel="returnSelectAllCancel"
+      @table-delete-detail="returnDeleteDetail"
+    />
+    <businessButton :btn-config="btnConfig" />
+  </div>
+</template>
+
+<script>
+  import transferaudit from '@/js/modal/InventoryCenter/transferaudit';
+
+  export default transferaudit;
+</script>
+
+<style>
+  @import "~@/css/modal/InventoryCenter/transferaudit.less";
+</style>
