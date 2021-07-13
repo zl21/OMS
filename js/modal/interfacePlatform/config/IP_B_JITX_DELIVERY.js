7cf0e96f83c199e107d770ca0454db1da4880d77
liushuanglian <li.sl@burgeon.cn>
2021-07-13 13:51:24 +0800

    v1.9.41 | i18n orderTime

--- js/modal/interfacePlatform/config/IP_B_JITX_DELIVERY.js
+++ js/modal/interfacePlatform/config/IP_B_JITX_DELIVERY.js
@@ -75,7 +75,7 @@ export default {
         style: 'date',
         type: 'datetimerange', // 日期组件类型,默认为data  (daterange)为双日期区间选择
         value: 'query_date',
-        label: $i18n.t('form_label.queryTime'), // 查询时间
+        label: $i18n.t('form_label.orderTime'), // 下单时间
         width: '24',
         format: 'yyyy-MM-dd HH:mm:ss', // 格式参照burgeonui
         placeholder: ''
