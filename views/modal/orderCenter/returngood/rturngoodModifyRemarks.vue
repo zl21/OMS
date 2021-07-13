f6b974b275beebf2ee73b35181784e21ea410e49
weixiaoxiao <1655654318@qq.com>
2021-07-13 11:34:20 +0800

    bug:43166

--- views/modal/orderCenter/returngood/rturngoodModifyRemarks.vue
+++ views/modal/orderCenter/returngood/rturngoodModifyRemarks.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="bounced">
-    <Form :label-width="65">
-      <FormItem label="修改备注">
+  <div class="modifyRemark customized-modal">
+    <Form :label-width="80" :model="bouncedData" :rules="ruleValidate">
+      <FormItem label="修改备注"  prop="value">
         <Input
           v-model="bouncedData.value"
           type="textarea"
           :rows="4"
           placeholder="请输入"
         />
+      </FormItem>
+      <FormItem>
         <RadioGroup v-model="bouncedData.animal">
           <Radio label="覆盖原备注" />
           <Radio label="追加到原备注" />
         </RadioGroup>
       </FormItem>
     </Form>
-    <jordanBtn :btn-config="btnConfig" />
+    <jordanBtn  class="modal-footer" :btn-config="btnConfig" />
   </div>
 </template>
 
