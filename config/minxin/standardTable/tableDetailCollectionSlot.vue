fa3d8cdf2a1960428845a2d10fa9e8f028f74d68
weixiaoxiao <1655654318@qq.com>
2021-08-09 19:16:41 +0800

    标准的详情页修改

--- config/minxin/standardTable/tableDetailCollectionSlot.vue
+++ config/minxin/standardTable/tableDetailCollectionSlot.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-18 10:08:27
- * @LastEditTime: 2021-07-01 13:50:00
+ * @LastEditTime: 2021-08-09 19:15:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /front-standard-product/src/config/minxin/tableDetailCollectionSlot.vue
@@ -32,12 +32,15 @@
 </script>
 <style lang="less" scoped>
 @import '~@burgeon/oms-theme/skin/public.less';
-.detail-content {
-  height: 40px;
-  #bundle > .flex(@justify:center,@align:center)
+.TableDetailCollection{
+  .detail-content {
+    height: 40px;
+    #bundle > .flex(@justify:center,@align:center)
+  }
+  .detail-operation{
+    #bundle > .flex(@justify:space-between,@align:center);
+    // margin-bottom: @base-mr;
+  }
 }
-.detail-operation{
-  #bundle > .flex(@justify:space-between,@align:center);
-  // margin-bottom: @base-mr;
-}
+
 </style>
