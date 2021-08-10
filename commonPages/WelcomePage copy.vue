351c5eacba19d6ce88dcf50810ac2c8a41cf2abe
weixiaoxiao <1655654318@qq.com>
2021-08-10 23:36:37 +0800

    | v1.11.75 | form表单定制+标准收缩按钮定位 |

---
+++ commonPages/WelcomePage copy.vue
@@ -0,0 +1,438 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-27 11:20:18
+ * @LastEditTime: 2021-08-10 19:24:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /project-logic/commonPages/WelcomePage.vue
+-->
+<template>
+  <div class="monitoringPlatform">
+    <div class="header">
+      <!-- <span>11</span>
+      <span>22</span>
+      <span>33</span> -->
+      <!-- <businessButton :btn-config="data.header.btn" class="btn" /> -->
+      <div class="btn">
+        <Button type="primary">近三天</Button>
+        <Button type="text">昨天</Button>
+        <Button type="text">当天</Button>
+      </div>
+      <div class="time">
+        <span>更新时间：{{ data.header.time }}</span>
+        <Icon type="ios-refresh" />
+      </div>
+    </div>
+    <div class="main">
+      <div class="mainContent main01">
+        <h2>{{ data.main01.title }}</h2>
+        <div class="main01body">
+          <div class="left">
+            <div id="main" style="width: 200px; height: 200px"></div>
+          </div>
+          <div class="right">
+            <div
+              class="rightItem"
+              v-for="(it, index) in data.main01.data"
+              :key="index"
+              :class="it.status == 0 ? 'abnormal' : 'normal'"
+            >
+              <Icon type="ios-alert-outline" />
+              <span>{{ it.title }}</span>
+              <div class="info">
+                <span>{{ it.message }}</span>
+                <span>{{ it.sum }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div :class="['mainContent', 'main02', up]">
+        <h2>{{ data.main02.title }}</h2>
+        <div class="btn">
+          <Button type="primary">全部</Button>
+          <Button type="text">异常</Button>
+          <Button type="text" :icon="m2BtnIcon" @click="upDown"></Button>
+        </div>
+        <div class="main02body">
+          <div
+            class="m2Item"
+            v-for="(it, index) in data.main02.data"
+            :key="index"
+            :class="it.status == 0 ? 'abnormal' : 'normal'"
+          >
+            <div class="title">
+              <Icon :type="it.titleIcon" />
+              <span>{{ it.title }}</span>
+              <Icon :type="it.statusIcon" class="warnIcon" />
+            </div>
+            <div
+              for=""
+              v-for="(i, j) in it.data"
+              :key="'m2' + j"
+              class="itemData"
+            >
+              <span>{{ i.A_name }}</span>
+              <span>{{ i.A_sum }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div :class="['mainContent', 'main03', up]">
+        <h2>{{ data.main03.title }}</h2>
+        <div class="btn">
+          <Button type="primary">全部</Button>
+          <Button type="text">异常</Button>
+          <Button type="text" :icon="m3BtnIcon"></Button>
+        </div>
+        <div class="main03body">
+          <div
+            class="m3Item"
+            v-for="(it, index) in data.main03.data"
+            :key="index"
+            :class="it.status == 0 ? 'abnormal' : 'normal'"
+          >
+            <div class="title">
+              <span>{{ it.title }}</span>
+              <span>{{ it.time }}</span>
+            </div>
+            <div class="itemData"></div>
+          </div>
+        </div>
+      </div>
+      <div class="mainContent main04">
+        <h2>{{ data.main04.title }}</h2>
+        <div class="btn">
+          <Button type="primary">近三天</Button>
+          <Button type="text">昨天</Button>
+          <Button type="text">当天</Button>
+        </div>
+        <div id="brokenLine" style='width:100%;height:400px'></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import businessButton from 'professionalComponents/businessButton';
+import dateUtil from '@/assets/js/__utils__/date.js';
+import * as echarts from 'echarts';
+
+export default {
+  name: 'WelcomePage',
+  components: {
+    businessButton,
+  },
+  data() {
+    return {
+      up: '',
+      m2BtnIcon: 'ios-arrow-down',
+      m3BtnIcon: 'ios-arrow-down',
+      data: {
+        header: {
+          time: new Date(),
+          btn: {
+            btnsite: 'left', // 按钮对齐方式
+            typeAll: 'default',
+            buttons: [
+              {
+                webname: 'threeDays',
+                text: '近三天',
+                type: 'primary',
+                btnclick: () => {
+                },
+              },
+              {
+                webname: 'yesterday',
+                text: '昨天',
+                btnclick: () => {
+                },
+              },
+              {
+                webname: 'today',
+                text: '当天',
+                btnclick: () => {
+                },
+              }
+            ]
+          },
+        },
+        main01: {
+          title: '第三方监控',
+          sum: 10,
+          ex_sum: 2,
+          normal_sum: 8,
+          status: 0,
+          status_name: '异常',
+          data: [
+            {
+              status: 0,
+              title: 'AG项目1',
+              message: '异常单据',
+              sum: 288,
+            },
+            {
+              status: 1,
+              title: 'AG项目2',
+              message: '异常单据',
+              sum: 288,
+            },
+            {
+              status: 1,
+              title: 'AG项目3',
+              message: '异常单据',
+              sum: 288,
+            },
+            {
+              status: 0,
+              title: 'AG项目4',
+              message: '异常单据',
+              sum: 288,
+            }
+          ]
+        },
+        main02: {
+          title: '对外接口监控项目详细数据',
+          data: [
+            {
+              titleIcon: 'ios-home',
+              title: 'WMS(AG项目1)',
+              statusIcon: 'ios-alert-outline',
+              data: [
+                {
+                  status: 0,
+                  A_name: '推送总数',
+                  A_sum: 3880,
+                },
+                {
+                  status: 1,
+                  A_name: '推送成功',
+                  A_sum: 345,
+                },
+                {
+                  status: 0,
+                  A_name: '推送失败率',
+                  A_sum: '20%',
+                },
+              ],
+            },
+            {
+              titleIcon: 'ios-home',
+              title: 'WMS(AG项目1)',
+              statusIcon: 'ios-alert-outline',
+              data: [
+                {
+                  status: 0,
+                  A_name: '推送总数',
+                  A_sum: 3880,
+                },
+                {
+                  status: 1,
+                  A_name: '推送成功',
+                  A_sum: 345,
+                },
+                {
+                  status: 0,
+                  A_name: '推送失败率',
+                  A_sum: '20%',
+                },
+              ],
+            },
+            {
+              titleIcon: 'ios-home',
+              title: 'WMS(AG项目1)',
+              statusIcon: 'ios-alert-outline',
+              data: [
+                {
+                  status: 0,
+                  A_name: '推送总数',
+                  A_sum: 3880,
+                },
+                {
+                  status: 1,
+                  A_name: '推送成功',
+                  A_sum: 345,
+                },
+                {
+                  status: 0,
+                  A_name: '推送失败率',
+                  A_sum: '20%',
+                },
+              ],
+            },
+            {
+              titleIcon: 'ios-home',
+              title: 'WMS(AG项目1)',
+              statusIcon: 'ios-alert-outline',
+              data: [
+                {
+                  status: 0,
+                  A_name: '推送总数',
+                  A_sum: 3880,
+                },
+                {
+                  status: 1,
+                  A_name: '推送成功',
+                  A_sum: 345,
+                },
+                {
+                  status: 0,
+                  A_name: '推送失败率',
+                  A_sum: '20%',
+                },
+              ],
+            },
+            {
+              titleIcon: 'ios-home',
+              title: 'WMS(AG项目1)',
+              statusIcon: 'ios-alert-outline',
+              data: [
+                {
+                  status: 0,
+                  A_name: '推送总数',
+                  A_sum: 3880,
+                },
+                {
+                  status: 1,
+                  A_name: '推送成功',
+                  A_sum: 345,
+                },
+                {
+                  status: 0,
+                  A_name: '推送失败率',
+                  A_sum: '20%',
+                },
+              ],
+            },
+            {
+              titleIcon: 'ios-home',
+              title: 'WMS(AG项目1)',
+              statusIcon: 'ios-alert-outline',
+              data: [
+                {
+                  status: 0,
+                  A_name: '推送总数',
+                  A_sum: 3880,
+                },
+                {
+                  status: 1,
+                  A_name: '推送成功',
+                  A_sum: 345,
+                },
+                {
+                  status: 0,
+                  A_name: '推送失败率',
+                  A_sum: '20%',
+                },
+              ],
+            },
+          ]
+        },
+        main03: {
+          title: '服务异常监控详细数据（AG项目1）',
+          data: [
+            {
+              title: '转单服务',
+              time: '2h',
+              status: 0,
+              min: 10,
+              max: 10000,
+              desc: '积压单量',
+              tips: '',
+            },
+            {
+              title: '转单服务',
+              time: '2h',
+              status: 0,
+              min: 10,
+              max: 10000,
+              desc: '积压单量',
+              tips: '',
+            },
+            {
+              title: '转单服务',
+              time: '2h',
+              status: 0,
+              min: 10,
+              max: 10000,
+              desc: '积压单量',
+              tips: '',
+            },
+            {
+              title: '转单服务',
+              time: '2h',
+              status: 0,
+              min: 10,
+              max: 10000,
+              desc: '积压单量',
+              tips: '',
+            },
+            {
+              title: '转单服务',
+              time: '2h',
+              status: 0,
+              min: 10,
+              max: 10000,
+              desc: '积压单量',
+              tips: '',
+            },
+          ]
+        },
+        main04: {
+          title: '接口异常趋势图',
+        },
+      },
+      vmI18n: window.vmI18n
+    };
+  },
+  methods: {
+    upDown() {
+      this.up = this.up ? '' : 'fadeIn';
+      this.m2BtnIcon = this.m2BtnIcon == 'ios-arrow-down' ? 'ios-arrow-up' : 'ios-arrow-down';
+    },
+  },
+  mounted() {
+    // const domContent = document.getElementById('content');
+    // domContent.style.padding = '0 0';
+    this.brokenLineFun();
+  },
+  destroyed() {
+    if (document.getElementById('content')) {
+      const domContent = document.getElementById('content');
+      domContent.style.padding = '0 15px';
+    }
+  },
+  methods:{
+    brokenLineFun(){
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById('brokenLine'));
+      // 绘制图表
+      let option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                axisLine:{ //x轴线
+                  lineStyle:{
+                      color: 'transparent' 
+                  }
+                }
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                smooth: true
+            }]
+      };
+      myChart.setOption(option);
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+@import "~@burgeon/oms-theme/skin/public.less";
+@import "./public.less";
+</style>
--- commonPages/layout/listFormButton.vue
+++ commonPages/layout/listFormButton.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-07-30 11:24:27
- * @LastEditTime: 2021-08-04 16:23:17
+ * @LastEditTime: 2021-08-10 23:31:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /project-logic/commonPages/layout/listFormButton.vue
@@ -9,8 +9,14 @@
 <template>
   <div>
     <Button size="small" @click="buttonClick"> 查找 </Button>
-    <Button type="primary" size="small" @click="buttonClick('reset')"> 重置 </Button>
-    <Button size="small" v-if="isShowSelectBut" @click="iconClick" :icon='icon'></Button>
+    <Button
+      type="primary"
+      size="small"
+      @click="buttonClick('reset')"
+    >
+      重置
+    </Button>
+    <!-- <Button class="positionArrow" size="small" v-if="isShowSelectBut" @click="iconClick" :icon='icon'></Button> -->
     <!-- {{slotProps.item.name}} -->
   </div>
 </template>
@@ -21,68 +27,100 @@ export default {
       type: Function,
       default: () => {
         //  点击事件回调
-      }
+      },
     },
     IconCallBack: {
       type: Function,
       default: () => {
         //  收拉回调
-      }
-    }
-
+      },
+    },
   },
   data() {
     return {
-      icon: 'ios-arrow-down',
-      hiddenButtons: ['reset', 'search'],  // 隐藏按钮的key
-      hiddenIcon: true, // 是否隐藏收拉icon
-      isShowSelectBut:false
-    }
+      icon: "ios-arrow-down",
+      hiddenButtons: ["reset", "search"], // 隐藏按钮的key
+      hiddenIcon: false, // 是否隐藏收拉icon
+      isShowSelectBut: false,
+    };
   },
-  computed:{
-    Foldnum:function () {
-      return this.$parent.searchFoldnum;
-    }
+  computed: {
+    // Foldnum:function () {
+    //   return this.$parent.searchFoldnum;
+    // }
   },
-  watch:{
-    Foldnum:{
-      handler(newName) {
-        let inputNum = this.$parent.formArray.length;
-        let isColumn = this.$parent.setdefaultColumn;
-        this.isShowSelectBut = (inputNum / isColumn) > newName ? true : false
-    　},
-    　immediate: true
-    }
+  watch: {
+    // Foldnum:{
+    //   handler(newName) {
+    //     let inputNum = this.$parent.formArray.length;
+    //     let isColumn = this.$parent.setdefaultColumn;
+    //     this.isShowSelectBut = (inputNum / isColumn) > newName ? true : false
+    // 　},
+    // 　immediate: true
+    // }
   },
-  mounted(){
-    
-  },
+  mounted() {},
   methods: {
     buttonClick(type) {
       this.ButttonCallBack(type);
     },
-    iconClick(e) { 
+    iconClick(e) {
       console.log(this.$parent.searchFoldnum);
-      if (e.target.className.includes('ios-arrow-down') || (e.target.children.length && e.target.children[0].className.includes('ios-arrow-down'))) {
-        this.icon = 'ios-arrow-up'
-      } else if (e.target.className.includes('ios-arrow-up') || (e.target.children.length && e.target.children[0].className.includes('ios-arrow-up'))) {
-        this.icon = 'ios-arrow-down'
+      if (
+        e.target.className.includes("ios-arrow-down") ||
+        (e.target.children.length &&
+          e.target.children[0].className.includes("ios-arrow-down"))
+      ) {
+        this.icon = "ios-arrow-up";
+      } else if (
+        e.target.className.includes("ios-arrow-up") ||
+        (e.target.children.length &&
+          e.target.children[0].className.includes("ios-arrow-up"))
+      ) {
+        this.icon = "ios-arrow-down";
       }
-      this.IconCallBack()
-    }
-
-  }
-}
+      this.IconCallBack();
+    },
+  },
+};
 </script>
 
-<style lang="less" scoped>
-@import '~@burgeon/oms-theme/skin/public.less';
+<style lang="less">
+@import "~@burgeon/oms-theme/skin/public.less";
+.standarTableListContent
+  .StandardTableListRootDiv
+  .downComponent-context
+  .tag-close {
+  width: 55px;
+  height: 14px;
+  background: transparent;
+  bottom: 0;
+  left: 50%;
+  top: auto;
+  right: auto;
+  transform: translateX(-50%);
+  i {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    background: url(../../assets/img/aroowDown.png) right no-repeat;
+    background-size: 100%;
+    transform:rotate(0);
+    &::before {
+      content: "";
+    }
+  }
+  .icon-xiadown {
+    background: url(../../assets/img/aroowUp.png) right no-repeat;
+    background-size: 100%;
+  }
+}
 .ListsForm-button {
   bottom: 24px;
   right: 43px;
-  button{
+  button {
     #bundle > .defalutBtn;
-    &:first-child{
+    &:first-child {
       #bundle > .primaryBtn;
     }
   }
