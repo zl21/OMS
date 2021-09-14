8a4c476577e3fff9d4df3ec9fdf1cf69fd14201b
liushuanglian <li.sl@burgeon.cn>
2021-09-14 17:49:31 +0800

    临时平台店铺商品表导入模板

--- src/service/modules/common.js
+++ src/service/modules/common.js
@@ -266,7 +266,7 @@ export default {
       { 
         tableName: 'SG_B_CHANNEL_PRODUCT_import',
         url: '/p/cs/product/productItemImport',
-        downloadUrl: 'http://sm-r3-upload-test.oss-cn-shanghai.aliyuncs.com/SG_TMALL_NEW/%E5%A4%A9%E7%8C%AB%E6%96%B0%E5%93%81%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF20210903.xlsx?OSSAccessKeyId=LTAI4GC4xAd2Z1w55piRSMva&Expires=2630663238&Signature=mTRE2mj5IYksm8d%2Bta04EDcrTQs%3D'
+        downloadUrl: 'http://sm-r3-upload-prod.oss-cn-hangzhou.aliyuncs.com/SG_TMALL_NEW/%E5%A4%A9%E7%8C%AB%E6%96%B0%E5%93%81%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF20210903.xlsx?OSSAccessKeyId=LTAI4GC4xAd2Z1w55piRSMva&Expires=1000001631612823&Signature=DXqYZhfyDRxf9s5JDE7u2YQy5CU%3D'
       } // 平台店铺商品表 - 导入
     ];
     return arr;
