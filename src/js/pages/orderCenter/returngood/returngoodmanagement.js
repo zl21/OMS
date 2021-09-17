c1e6525e9d33422f214785a26377bdfb30d2fbed
zhou.l <zhou.l@burgeon.cn>
2021-09-17 10:04:32 +0800

    优化：退换->新增->原始订单编号->添加明细多的订单明细加载到退换货单退货明细特别慢

--- src/js/pages/orderCenter/returngood/returngoodmanagement.js
+++ src/js/pages/orderCenter/returngood/returngoodmanagement.js
@@ -5,8 +5,9 @@ import businessActionTable from 'professionalComponents/businessActionTable';
 import businessLabel from 'professionalComponents/businessLabel';
 import { setTimeout } from 'timers';
 import businessDialog from 'professionalComponents/businessDialog';
-import publicMethodsUtil from '@/assets/js/public/publicMethods';
 import businessStatusFlag from 'professionalComponents/businessStatusFlag';
+import OrderItem from 'allpages/OrderCenter/returngood/orderItem.vue';
+import publicMethodsUtil from '@/assets/js/public/publicMethods';
 import { buttonPermissionsMixin } from '@/assets/js/mixins/buttonPermissions';
 import { dataAccessMixin } from '@/assets/js/mixins/dataAccess';
 import loading from '@/component/loading.vue';
@@ -14,7 +15,6 @@ import comUtils from '@/assets/js/__utils__/common';
 
 // import OrderItem from './orderItem';
 
-import OrderItem from 'allpages/OrderCenter/returngood/orderItem.vue';
 
 const areaList = require('@/assets/js/address/area-list');
 const { parse, parseArea } = require('@/assets/js/address/address-parse');
@@ -1227,7 +1227,7 @@ export default {
             webname: 'refund_jump_to_order',
             disabled: false, // 按钮禁用控制
             btnclick: () => {
-              const _this = this
+              const _this = this;
               comUtils.tabCloseAppoint(_this);
               _this.$store.commit('customize/TabHref', {
                 id: _this.information.formValue.ORIG_ORDER_ID,
@@ -1329,7 +1329,7 @@ export default {
               if (item.REFUND_STATUS != 6) queryList.push(item);
             });
             const newQueryList = [];
-            
+
             queryList.forEach(subitem => {
               const newItem = {};
               newItem.reserve_bigint10 = subitem.ID;
@@ -1425,7 +1425,7 @@ export default {
                   document.getElementsByClassName('burgeon-select-selected-value')[1].className = 'burgeon-select-selected-value inputBgcolor';
                 }, 10);
               }
-              
+
               _this.replacement.formData[5].itemdata.pid = res.data.data.returnOrders.RECEIVER_PROVINCE_ID;
               _this.replacement.formData[6].itemdata.pid = res.data.data.returnOrders.RECEIVER_CITY_ID;
               _this.replacement.formData[7].itemdata.pid = res.data.data.returnOrders.RECEIVER_AREA_ID;
@@ -1434,6 +1434,7 @@ export default {
               // 设置水印
               _this.statusName = _this.waterMarkMap[_this.status] ? _this.waterMarkMap[_this.status] : 'Watermark to be added';
               _this.defectiveList = res.data.data.orderDefects;
+              await this.queryBounced(res.data.data.refundDtoList[0].OC_B_ORDER_ID);
               for await (const tempItem of res.data.data.refundDtoList) {
                 tempItem.PRODUCT_MARK = tempItem.PRODUCT_MARK == 1 ? '正品' : '次品';
                 tempItem.amt_refund_single = tempItem.AMT_REFUND_SINGLE;
@@ -1673,7 +1674,7 @@ export default {
       _this.jordanTableConfig.loading = true;
       this.information.formData[4].style = 'input';
       _this.service.orderCenter.findDetail({
- id: _this.$route.query.id, start: 1, count: 50, isRefund2Exchange: this.$route.query.flag == 'RefundToExchange' ? 1 : undefined 
+ id: _this.$route.query.id, start: 1, count: 50, isRefund2Exchange: this.$route.query.flag == 'RefundToExchange' ? 1 : undefined
 }).then(async res => {
         if (res.data.code === 0) {
           _this.jordanTableConfig.loading = false;
@@ -1681,9 +1682,9 @@ export default {
           // 是否无名件匹配
           if (res.data.data.returnOrders.IS_ANONYMOUS != null) {
             if (res.data.data.returnOrders.IS_ANONYMOUS == 1) {
-              _this.information.formValue.IS_ANONYMOUS = '是'
+              _this.information.formValue.IS_ANONYMOUS = '是';
             } else if (res.data.data.returnOrders.IS_ANONYMOUS == 0) {
-              _this.information.formValue.IS_ANONYMOUS = '否'
+              _this.information.formValue.IS_ANONYMOUS = '否';
             }
           }
           _this.information.formValue.OC_B_REFUND_IN_ID = res.data.data.returnOrders.OC_B_REFUND_IN_ID; // 退货入库单编号
@@ -1714,7 +1715,7 @@ export default {
             _this.reconstructionGetDetail(tempRefundDtoList[i], tempRefundDtoList[i]);
           }
 
-          
+
           res.data.data.refundDtoList = tempRefundDtoList;
           for (let i = 0; i < res.data.data.exchangeDtoList.length; i++) {
             const item = res.data.data.exchangeDtoList[i];
@@ -3270,9 +3271,9 @@ export default {
       // 是否无名件匹配
       if (copyFormValue.IS_ANONYMOUS) {
         if (copyFormValue.IS_ANONYMOUS == '是') {
-          copyFormValue.IS_ANONYMOUS = 1
+          copyFormValue.IS_ANONYMOUS = 1;
         } else if (copyFormValue.IS_ANONYMOUS == '否') {
-          copyFormValue.IS_ANONYMOUS = 0
+          copyFormValue.IS_ANONYMOUS = 0;
         }
       }
       const params = {
@@ -3280,7 +3281,7 @@ export default {
         OcBreturnOrder: Object.assign(copyFormValue, _this.replacement.formValue, money), // 主表信息
         OcBreturnOrderExchange: Elist, // 换货明细
         OcBreturnOrderRefund: Rlist, // 退货明细
-        isRefund2Exchange 
+        isRefund2Exchange
       };
       // 复制订单标识
       if (_this.$route.query.cloneReturnGoodId) params.copytype = 1;
@@ -4346,7 +4347,8 @@ export default {
     async reconstructionGetDetail(item, returnItem, ecode = '') {
       // 重构明细拉取请求, 由于项目时间比较紧, 目前只修改了部分接口, 老接口结构保持不变;
       if (ecode != '') {
-        await this.getDataByProinfo(ecode, 1);
+        // await this.getDataByProinfo(ecode, 1);
+        this.getDataByProInfoList(ecode);
         returnItem.clrList = this.clrListArr;
         returnItem.sizeList = this.sizeListArr;
       } else {
@@ -4354,6 +4356,23 @@ export default {
         returnItem.sizeList = item.selected.psCSpec2objList;
       }
       // return returnItem;
+    },
+    getDataByProInfoList(ecode) {
+      const _this = this;
+      const list = this.order.table.data[0].QUERYORDERITEMRESULTLIST;
+      const proInfo = list.find(x => x.ecode == ecode);
+      if (proInfo) {
+        _this.clrListArr = proInfo.colorEntityList.map((x) => ({
+          psCSpec1objName: x.clrs,
+          psCSpec1objId: x.clrsId,
+          psCSpec1objCode: x.clrsEcode
+        }));
+        _this.sizeListArr = proInfo.sizeEntityList.map((x) => ({
+              psCSpec2objId: x.sizeId,
+              psCSpec2objCode: x.sizeEcode,
+              psCSpec2objName: x.sizes
+            }));
+      }
     },
     async getDataByProinfo(proEcode, dataType, sizeId, clrId) {
       const _this = this;
