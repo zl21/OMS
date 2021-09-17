e6d5095ab87ae1c3e5cf68c37fb256fe59fd4c77
zhou.l <zhou.l@burgeon.cn>
2021-09-17 11:27:21 +0800

    修改Pos管控仓-修改判断选中单据是否管控仓的字段-IS_POS

--- src/js/pages/orderCenter/returngood/returngoodmanagementList.js
+++ src/js/pages/orderCenter/returngood/returngoodmanagementList.js
@@ -393,18 +393,13 @@ export default {
                 this.$Message.error('仅支持对单个单据执行操作！');
                 return;
               }
-              /**
-               * "CP_C_PHY_WAREHOUSE_IN_ID_NAME" - "入库实体仓库"
-               * "CP_C_STORE_ENAME" - "入库逻辑仓库"
-               */
               const it = selectArr[0];
               // ✧ 校验退换货单的入库仓库是否为pos管控仓，若否，则提示：“仅支持对pos管控仓的仓库进行物流信息的修改！”
-              /* 
-              if (it.CP_C_PHY_WAREHOUSE_IN_ID_NAME != 'pos管控仓') {
+              console.log('oc_b_return_order_pos_incustomize::', it);
+              if (it.IS_POS != '1') {
                 this.$Message.error('仅支持对pos管控仓的仓库进行物流信息的修改！');
-                // return
+                return
               }
-              */
               let flag = false;
               if (it.RETURN_STATUS_NAME == '待退货入库' && ['未传', '成功', '失败'].includes(it.IS_TOWMS)) {
                 // ✧ 校验退换货单的单据状态是否为“待退货入库”&传wms状态=未传、成功、失败，若否，则提示：“仅支持对待退货入库，传wms状态=未传/成功/失败”进行操作！”
