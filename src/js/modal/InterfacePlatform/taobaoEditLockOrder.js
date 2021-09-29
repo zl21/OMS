d217d7f5899ddcd23a76cb64f62b211c23c7e5ce
王金铁 <wang.jt@burgeon.cn>
2021-09-29 11:23:34 +0800

    feat: taobaoEditLockOrder 锁单

--- src/js/modal/InterfacePlatform/taobaoEditLockOrder.js
+++ src/js/modal/InterfacePlatform/taobaoEditLockOrder.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import dayjs from 'dayjs';
-import BusinessForm from 'professionalComponents/businessForm';
+// import BusinessForm from 'professionalComponents/businessForm';
 import BusinessBtn from 'professionalComponents/businessButton';
 
 export default {
 	components: {
-		BusinessForm,
+		// BusinessForm,
 		BusinessBtn,
 	},
 	props: {
@@ -27,11 +27,11 @@ export default {
 	},
 	data() {
 		return {
+			// pageLoad: false,
 			form: {
 				status: 0,
 				time: null
 			},
-			pageLoad: false,
 			btnConfig: {
 				typeAll: 'error', // 按钮统一风格样式
 				btnsite: 'right', // 按钮位置 (right , center , left)
@@ -58,107 +58,85 @@ export default {
 					}
 				]
 			},
-			formConfig: {
-				formValue: {
-					numNumber: ''
-				},
-				formData: [
-					{
-						style: 'select', // 下拉框类型
-						label: '拒绝换货原因', // 下拉框前的值
-						width: '24', // 所占宽度宽度
-						value: 'refuseReasonId', // 输入框的值
-						multiple: false,
-						options: [
-							{ value: 4001, label: '库存不足或商品已下架' },
-							{ value: 4002, label: '申请时间已超7天无理由退换货时限' },
-							{ value: 4003, label: '定制商品不支持换货' },
-							{ value: 4004, label: '不符合商品完好标准' },
-							{ value: 4005, label: '非包邮商品运费未达成一致' },
-							{ value: 4006, label: '与买家达成一致' },
-							{ value: 4016, label: '所换商品差价，未协商一致' },
-							{ value: 153, label: '其他' }
-						]
-					},
-					{
-						style: 'textarea', // 输入框类型
-						label: '拒绝换货原因', // 输入框前文字
-						value: 'outRefuseCopywriting', // 输入框的值
-						width: '24', // 所占的宽度 (宽度分为24份,数值代表所占份数的宽度)
-						disabled: false,
-						autosize: { minRows: 3, maxRows: 3 }
-					}
-				],
-				// 表单非空提示
-				ruleValidate: {
-					refuseReasonId: [{ required: true, message: ' ', trigger: 'blur' }]
-				}
-			}
 		};
 	},
 	mounted() {
-		const formData = new FormData();
-		formData.append('param', '{}');
-		this.pageLoad = true;
-		axios({
-			url: '/p/cs/exchangeRefuseReason',
-			method: 'post',
-			data: formData
-		}).then(res => {
-			this.pageLoad = false;
-			if (res.data.code === 1) {
-				const resData = JSON.parse(res.data.datas);
-				this.formConfig.formData[0].options = resData.map(val => ({
-					label: val.outRefuseCopywriting,
-					value: val.refuseReasonId
-				}));
-			} else {
-				this.$message.error('拒绝换货原因请求失败');
-			}
-		}).catch(() => {
-			this.pageLoad = false;
-		});
+		this.timeChange(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+		// const formData = new FormData();
+		// formData.append('param', '{}');
+		// this.pageLoad = true;
+		// axios({
+		// 	url: '/p/cs/exchangeRefuseReason',
+		// 	method: 'post',
+		// 	data: formData
+		// }).then(res => {
+		// 	this.pageLoad = false;
+		// 	if (res.data.code === 1) {
+		// 		const resData = JSON.parse(res.data.datas);
+		// 		this.formConfig.formData[0].options = resData.map(val => ({
+		// 			label: val.outRefuseCopywriting,
+		// 			value: val.refuseReasonId
+		// 		}));
+		// 	} else {
+		// 		this.$message.error('拒绝换货原因请求失败');
+		// 	}
+		// }).catch(() => {
+		// 	this.pageLoad = false;
+		// });
 	},
 	methods: {
+		/**
+		 * 预计解锁时间
+		 * @param {string} time
+		 * @param {string} type
+		 */
 		timeChange(time, type) {
-			this.form.time = time;
+			const _time = dayjs(time).valueOf(),
+				currentTime = dayjs().valueOf(),
+				currentTimeStr = dayjs(currentTime).format('YYYY-MM-DD HH:mm:ss');
+			this.form.time = currentTime >= _time ? currentTimeStr : time;
 		},
 
 		confirmChange() {
-			const formValue = this.formConfig.formValue;
-			if (!formValue.refuseReasonId) {
-				this.$message.warning('拒绝换货原因不能为空');
-				return;
-			}
-			if (this.idArray.length === 0) {
-				this.$message.error('请先选择需要拒绝换货的单据！');
-				return;
-			}
 			const param = {
-				ids: this.idArray,
-				menu: '淘宝换货单接口',
-				refuseReasonId: formValue.refuseReasonId,
-				outRefuseCopywriting: formValue.outRefuseCopywriting
-			};
-			const fromdata = new FormData();
-			fromdata.append('param', JSON.stringify(param));
-			this.pageLoad = true;
-			axios({
-				url: '/p/cs/exchangeRefuse',
-				method: 'post',
-				data: fromdata
-			}).then(res => {
-				this.pageLoad = false;
-				if (res.data.code === 0) {
-					this.$message.success(res.data.message);
-					this.$emit('closeActionDialog', true);
-				} else {
-					// this.$message.error(res.data.message || '拒绝换货失败');
-					this.$emit('closeActionDialog', true);
-				}
-			}).catch(() => {
-				this.pageLoad = false;
-			});
+				...this.form,
+				ids: this.idArray
+			}
+			console.log('param: ', param);
+			// const formValue = this.formConfig.formValue;
+			// if (!formValue.refuseReasonId) {
+			// 	this.$message.warning('拒绝换货原因不能为空');
+			// 	return;
+			// }
+			// if (this.idArray.length === 0) {
+			// 	this.$message.error('请先选择需要拒绝换货的单据！');
+			// 	return;
+			// }
+			// const param = {
+			// 	ids: this.idArray,
+			// 	menu: '淘宝换货单接口',
+			// 	refuseReasonId: formValue.refuseReasonId,
+			// 	outRefuseCopywriting: formValue.outRefuseCopywriting
+			// };
+			// const fromdata = new FormData();
+			// fromdata.append('param', JSON.stringify(param));
+			// this.pageLoad = true;
+			// axios({
+			// 	url: '/p/cs/exchangeRefuse',
+			// 	method: 'post',
+			// 	data: fromdata
+			// }).then(res => {
+			// 	this.pageLoad = false;
+			// 	if (res.data.code === 0) {
+			// 		this.$message.success(res.data.message);
+			// 		this.$emit('closeActionDialog', true);
+			// 	} else {
+			// 		// this.$message.error(res.data.message || '拒绝换货失败');
+			// 		this.$emit('closeActionDialog', true);
+			// 	}
+			// }).catch(() => {
+			// 	this.pageLoad = false;
+			// });
 		}
 	}
 };
