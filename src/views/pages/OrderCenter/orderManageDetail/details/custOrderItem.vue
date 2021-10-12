e53ea993d32fb68c06d8bce0804621c170d65b2f
魏萧萧 <1655654318@qq.com>
2021-10-12 15:28:56 +0800

    禁止点击遮罩层 关闭

--- src/views/pages/OrderCenter/orderManageDetail/details/custOrderItem.vue
+++ src/views/pages/OrderCenter/orderManageDetail/details/custOrderItem.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-11 11:58:02
- * @LastEditTime: 2021-10-12 15:00:21
+ * @LastEditTime: 2021-10-12 15:28:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /r3-web-senmir-v1.4/src/views/pages/OrderCenter/orderManageDetail/details/custOrderItem.vue
@@ -20,7 +20,7 @@
       @on-page-change="pageChange"
       @on-page-size-change="pageSizeChange"
     />
-    <Modal v-model="modal" title="库存查询" draggable mask width="600">
+    <Modal v-model="modal" title="库存查询" draggable mask  :mask-closable="false" width="600">
       <ul class="stockInfo">
         <li v-for="(item, index) in modalTable.stockInfo" :key="index">
           <label>{{ item.label }}:</label><span>{{ item.value }}</span>
