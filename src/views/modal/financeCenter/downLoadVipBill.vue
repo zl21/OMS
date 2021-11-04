46d8fb488e5654d16051ffe697550726aa1bb59e
段若愚 <duan.ry@burgeon.cn>
2021-11-04 18:27:52 +0800

    chore: 优化编译脚本
    1. 移除clean-webpack-plugin 转换成webpack 内置的参数;
    
    2. 添加特定的webpack-dev-server 输出成果;

--- src/views/modal/financeCenter/downLoadVipBill.vue
+++ src/views/modal/financeCenter/downLoadVipBill.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-03 18:01:58
+ * @LastEditTime: 2021-11-04 15:47:59
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /burgeon-project-logic/src/views/modal/financeCenter/downLoadVipBill.vue
+-->
 <template>
   <!-- 下载账单 -->
   <div
@@ -5,7 +13,7 @@
     style="width:430px;padding-right:20px"
   >
     <businessForm :form-config="downLoadFormConfig" />
-    <businessBtn :btn-config="downLoadBtnConfig" />
+    <businessButton :btn-config="downLoadBtnConfig" />
   </div>
 </template>
 
