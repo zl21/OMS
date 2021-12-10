86bfe313f9955ba3664251256eafe2864517680f
zhou.l <zhou.l@burgeon.cn>
2021-12-10 19:33:10 +0800

    theme test

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,12 +2,33 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming';
 import img from '../static/img/icon.svg'
+import logo from '../favicon.ico'
 
 addons.setConfig({
     theme: create({
         base: 'light',
         brandTitle: 'Oms UI',
         // brandUrl: 'https://example.com',
-        brandImage: img,
+        brandImage: logo,
     }),
+    isFullscreen: false,
+    showNav: true,
+    showPanel: true,
+    panelPosition: 'bottom',
+    enableShortcuts: true,
+    isToolshown: true,
+    // theme: undefined,
+    selectedPanel: undefined,
+    initialActive: 'sidebar',
+    sidebar: {
+        showRoots: true,
+        collapsedRoots: ['other'],
+    },
+    toolbar: {
+        title: { hidden: false, },
+        zoom: { hidden: false, },
+        eject: { hidden: false, },
+        copy: { hidden: false, },
+        fullscreen: { hidden: false, },
+    },
 });
