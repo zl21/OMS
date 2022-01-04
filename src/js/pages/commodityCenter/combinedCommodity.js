f4a30df39a7df2bd8b459948fef3f8ff26040fde
suzy <730318579@qq.com>
2022-01-04 19:25:04 +0800

    少了一个！

--- src/js/pages/commodityCenter/combinedCommodity.js
+++ src/js/pages/commodityCenter/combinedCommodity.js
@@ -1304,7 +1304,7 @@ export default {
           this.jordanTableConfig1.data[this.clickIndex].psCSkugroupList.push(this.t_data);
         }
       }
-      if (this.jordanTableConfig1.data.length || (this.jordanTableConfig1.data && this.jordanTableConfig1.data[this.clickIndex].psCSkugroupList.length < 1)) {
+      if (!this.jordanTableConfig1.data.length || (this.jordanTableConfig1.data && this.jordanTableConfig1.data[this.clickIndex].psCSkugroupList.length < 1)) {
         // this.$Message.warning("请选择点击要录入商品明细的条码信息");
         this.$Message.warning($it('tip.v5'));
         return;
