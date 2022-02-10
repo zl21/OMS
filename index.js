523a2167a9fed3543eac065d9311d1023fc60e06
liushuanglian <li.sl@burgeon.cn>
2022-02-10 13:51:01 +0800

    auto install

--- index.js
+++ index.js
@@ -6,50 +6,50 @@
  * @Description: In User Settings Edit
  * @FilePath: /burgeon-business-components/src/index.js
  */
-let comJS = require.context('burgeonComponents/common/js/', false, /\.js$/);
+let comJS = require.context('burgeonComponents/common/js/', false, /\.js$/)
 
 const Utils = {}
 comJS.keys().forEach(key => {
-	if (key == './utils.js') {
-		Utils.CM = comJS(key).default
-	} else {
-		const obj = comJS(key).default;
-		Object.assign(Utils, obj)
-	}
+  if (key == './utils.js') {
+    Utils.CM = comJS(key).default
+  } else {
+    const obj = comJS(key).default
+    Object.assign(Utils, obj)
+  }
 })
 Utils.unZip = require('./src/common/js/zip/index').default
 
-const context = require.context('burgeonComponents/view/', false, /\.vue$/);
-const Components = Utils.CM.exportModules(context);
+const context = require.context('burgeonComponents/view/', false, /\.vue$/)
+const Components = Utils.CM.exportModules(context)
 
-let directiveFiles = require.context('burgeonComponents/directive/', false, /\.js$/);
+let directiveFiles = require.context('burgeonComponents/directive/', false, /\.js$/)
 const Directives = Utils.CM.exportModules(directiveFiles)
 
-// const install = function (Vue, opts = {}) {
-// 	context.keys().forEach((key) => {
-// 		const component = context(key).default;
-// 		Vue.component(component.name, component)
-// 	});
+const install = function (Vue, opts = {}) {
+  context.keys().forEach(key => {
+    const component = context(key).default
+    Vue.component(component.name, component)
+  })
 
-// 	Object.keys(Directives).forEach(key => {
-// 		Vue.directive(key, Directives[key])
-// 	})
+  Object.keys(Directives).forEach(key => {
+    Vue.directive(key, Directives[key])
+  })
 
-// 	window.$utils = Utils.CM;
-// 	Vue.prototype.$utils = Utils.CM;
-// }
+  window.$utils = Utils.CM
+  Vue.prototype.$utils = Utils.CM
+}
 
 const BC = {
-	// install,
-	Components,
-	Utils,
-	name: '@burgeon/business-components',
-	version: require('./package.json').version,
+  install,
+  Components,
+  Utils,
+  name: '@burgeon/business-components',
+  version: require('./package.json').version
 }
- 
-// auto install
-// if (typeof window !== 'undefined' && window.Vue) {
-//   install(window.Vue);
-// }
 
-export default BC;
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default BC
