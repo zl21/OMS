8f0cc131450d5f52f28e67fda46fb5ab1f778d28
zhou.l <zhou.l@burgeon.cn>
2022-03-08 13:44:10 +0800

    script pub

--- shell.js
+++ shell.js
@@ -1,42 +1,71 @@
 const fs = require('fs')
 const path = require('path')
-const filepath = path.resolve(__dirname, 'package.json')
+var https = require('follow-redirects').https
+const filepath = path.resolve(__dirname, 'README.md')
+const filepath1 = path.resolve(__dirname, 'package.json')
 const filepath2 = path.resolve(__dirname, '.npmrc')
-let content = ''
-const oriObj = {
-  s1: `"url": "git@gitee.com:product_centerfront_end/burgeon-private-npm.git"`,
-  s2: `"name": "@burgeon/business-components",`
-}
-const resObj = {
-  s1: `"url": "git+https://github.com/zl21/BC_StoryBook.git"`,
-  s2: `"name": "@alanh/oms-ui",`
-}
-// fs.copyFileSync('./package.json', './package.copy.json')
+var content = '',
+  text = '',
+  title = ''
+// fs.copyFileSync('./package.json', './package.copy.json') */
 fs.readFile(filepath, (err, data) => {
-  if (err) {
-    return
-  }
+  if (err) return
   content = data.toString()
-  for (const key in oriObj) {
-    if (content.includes(oriObj[key])) {
-      console.log(oriObj[key])
-      content = content.replace(oriObj[key], resObj[key])
-    }
-  }
-  fs.writeFile(filepath, content, { flag: 'w' }, err => {
-    if (err) {
-      return
-    }
-  })
 })
-fs.readFile(filepath2, (err, data) => {
-  if (err) {
-    return
-  }
-  const content2 = `registry=https://registry.npmjs.org/`
+fs.readFile(filepath1, (err, data) => {
+  if (err) return
+  pack = JSON.parse(data.toString())
+  title = `@bojun/oms-ui@${pack.version} 发版啦！`
+})
+
+/* fs.readFile(filepath2, (err, data) => {
+  if (err) return
+  const content2 = "registry=https://registry.npmjs.org/:_authToken=${NPM_TOKEN}"
   fs.writeFile(filepath2, content2, { flag: 'w' }, err => {
     if (err) {
       return
     }
   })
+}) */
+var options = {
+  method: 'POST',
+  hostname: 'oapi.dingtalk.com',
+  path: '/robot/send?access_token=39ac3d54c6c7ef20d620ff213592c4eae9c810f7aa46c77f5e72855b94a24f14',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  maxRedirects: 20
+}
+
+var req = https.request(options, function (res) {
+  var chunks = []
+
+  res.on('data', function (chunk) {
+    chunks.push(chunk)
+  })
+
+  res.on('end', function (chunk) {
+    var body = Buffer.concat(chunks)
+    console.log(body.toString())
+  })
+
+  res.on('error', function (error) {
+    console.error(error)
+  })
 })
+
+setTimeout(e => {
+  text = content.split('|').slice(-3).join('|')
+  const body = {
+    msgtype: 'link',
+    link: {
+      text,
+      title,
+      picUrl: 'https://image.dbbqb.com/202203031702/563c99e7874db554e7b6e35b2199a2ef/O33Xo',
+      messageUrl: 'http://8.142.133.189:28880/'
+    }
+  }
+  var postData = JSON.stringify(body)
+  req.write(postData)
+  req.end()
+}, 1000)
