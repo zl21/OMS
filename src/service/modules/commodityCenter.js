41053d39be1e8088352d1f58abc61fa7b2056a32
hongyu <hongyu@ecsemir.com>
2022-03-23 17:11:29 +0800

    增加猫超商家修改

--- src/service/modules/commodityCenter.js
+++ src/service/modules/commodityCenter.js
@@ -11,4 +11,5 @@ export default {
   skuPage: params => network.post('/p/cs/product/skuPage', params), // 触发分页
   skuGroupDetailSearch: params => network.post('/p/cs/product/skuGroupDetailSearch', params), // 分页请求数据
   skuGroupEditorSearch: params => network.post('/p/cs/product/skuGroupEditorSearch', params), // 初始化数据
+  alibabaProductUpdate: params => network.post('/p/cs/alibabaProductUpdate', params), // 猫超商家仓修改
 };
