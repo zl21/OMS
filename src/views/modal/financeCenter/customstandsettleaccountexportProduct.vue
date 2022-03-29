142ff0e8b5f34578fcd1af1a8b645db4e67bd1a6
hongyu <hongyu@ecsemir.com>
2022-03-29 16:41:36 +0800

    O2O结算对账单新老品按钮添加

---
+++ src/views/modal/financeCenter/customstandsettleaccountexportProduct.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="width:400px">
+    <p class="custom-dialog-body-p">本次操作将提交至后台处理，是否确认提交？</p>
+    <p class="custom-dialog-body-p">提交后可通过<a href="/SYSTEM/TABLE/CP_C_TASK/24386">[我的任务]</a>进行查看</p>
+    <jordanBtn :btnConfig="exportBtnConfig"></jordanBtn>
+  </div>
+</template>
+
+<script>
+  import customstandsettleaccountexport from '@/js/modal/financeCenter/customstandsettleaccountexportProduct';
+
+  export default customstandsettleaccountexport;
+</script>
+
+<style lang="less" scoped>
+  .custom-dialog-body-p{
+    padding: 5px 0;
+  }
+</style>
