2cc2bdad4475f882f8951914f7be26f6f0a43b32
zhou.l <zhou.l@burgeon.cn>
2022-04-28 16:39:20 +0800

    fkinput-格式化一下

--- src/view/Fkinput.vue
+++ src/view/Fkinput.vue
@@ -1,44 +1,38 @@
 <template>
-  <div class="item-input item-col" :class="{ hidden: getHideColumn(itemdata) }">
-    <label v-if="itemdata.name && itemdata.notForm" class="title">
+  <div class="item-input item-col"
+    :class="{ hidden: getHideColumn(itemdata) }">
+    <label v-if="itemdata.name && itemdata.notForm"
+      class="title">
       <i v-if="itemdata.isnotnull">*</i>
       {{ itemdata.name }}:
     </label>
 
-    <Icon
-      v-if="!itemdata.readonly && (itemdata.fkdisplay === 'mrp' || itemdata.fkdisplay === 'drp') && itemdata.valuedata"
+    <Icon v-if="!itemdata.readonly && (itemdata.fkdisplay === 'mrp' || itemdata.fkdisplay === 'drp') && itemdata.valuedata"
       class="clear"
       type="ios-close-circle"
-      @click="clear"
-    />
-    <Tooltip
-      :placement="itemdata.placement || 'bottom'"
+      @click="clear" />
+    <Tooltip :placement="itemdata.placement || 'bottom'"
       :max-width="itemdata.maxwidth || 200"
       :content="itemdata.valuedata"
       :disabled="itemdata.istooltip ? !itemdata.valuedata : true"
       theme="light"
       class="fk-tooltip">
       <div v-if="itemdata.readonly || !isActive || isdisabled">
-          <input
-            v-model="itemdata.valuedata"
-            class="disabled table-input add-input"
-            type="text"
-            disabled
-          />
+        <input v-model="itemdata.valuedata"
+          class="disabled table-input add-input"
+          type="text"
+          disabled />
       </div>
 
-      
-      <div v-else style="position: relative">
+      <div v-else
+        style="position: relative">
         <!-- 遮挡输入建议组件的盒子，点击唤出下拉多选弹出框 -->
-        <span
-          v-if="itemdata.fkdisplay === 'mrp'"
+        <span v-if="itemdata.fkdisplay === 'mrp'"
           class="clickbox"
-          @click.stop="filterInputName(itemdata)"
-        />
+          @click.stop="filterInputName(itemdata)" />
         <!-- elementUI 的输入建议组件 -->
 
-        <el-autocomplete
-          v-if="itemdata.isfk"
+        <el-autocomplete v-if="itemdata.isfk"
           :ref="'autocomplete' + itemdata.colname"
           v-model="itemdata.valuedata"
           :disabled="itemdata.fkdisplay === 'mrp'"
@@ -53,35 +47,33 @@
           @change="inputChange(itemdata)"
           @keyup.native="inputKeyUp(itemdata, $event)"
           @keyup.enter.native="autocompleteEnter(itemdata, $event)"
-          @blur="autocompleteBlur(itemdata)"
-        >
+          @blur="autocompleteBlur(itemdata)">
           <!-- 模糊搜索弹出的搜索框结果 -->
           <template slot-scope="{ item }">
-            <div v-for="(value, key, index) of item" :key="index">
-              <span v-if="key == 'value'" :class="key" :title="value">{{
+            <div v-for="(value, key, index) of item"
+              :key="index">
+              <span v-if="key == 'value'"
+                :class="key"
+                :title="value">{{
                 value
               }}</span>
             </div>
           </template>
         </el-autocomplete>
 
-        <input
-          v-else
+        <input v-else
           v-model="itemdata.valuedata"
           class="table-input add-input"
           :class="itemdata.colname"
           :colname="itemdata.colname"
           type="text"
           @change="inputChange(itemdata)"
-          @keyup.enter="itemInputEnter($event)"
-        />
+          @keyup.enter="itemInputEnter($event)" />
 
-        <svg
-          v-if="itemdata.isfk && itemdata.fkdisplay === 'pop'"
+        <svg v-if="itemdata.isfk && itemdata.fkdisplay === 'pop'"
           class="ffish-icon danxuan"
           aria-hidden="true"
-          @click.stop="filterInputName(itemdata)"
-        >
+          @click.stop="filterInputName(itemdata)">
           <use xlink:href="#icon-danchuangdanxuan" />
         </svg>
         <!-- 弹出框，下拉多选的弹出框 -->
@@ -97,30 +89,21 @@
           title
           trigger="manual"
         > -->
-        <Poptip
-          v-if="
+        <Poptip v-if="
             itemdata.isfk &&
             (itemdata.fkdisplay == 'drp' || itemdata.fkdisplay == 'mrp')
           "
           v-model="popoverShow[itemdata.colname]"
           placement="bottom"
           class="popover-icon"
-          trigger="click"
-        >
-          <i
-            v-if="itemdata.fkdisplay === 'drp'"
+          trigger="click">
+          <i v-if="itemdata.fkdisplay === 'drp'"
             class="iconfont"
-            @click.stop="filterInputName(itemdata)"
-            >&#xe621;</i
-          >
-          <i
-            v-if="itemdata.fkdisplay === 'mrp'"
+            @click.stop="filterInputName(itemdata)">&#xe621;</i>
+          <i v-if="itemdata.fkdisplay === 'mrp'"
             class="iconfont"
-            @click.stop="filterInputName(itemdata)"
-            >&#xe6b9;</i
-          >
-          <fk-table
-            slot="content"
+            @click.stop="filterInputName(itemdata)">&#xe6b9;</i>
+          <fk-table slot="content"
             v-if="popoverShow[itemdata.colname]"
             class="view-fktable"
             :input-box="itemdata.fkdisplay == 'mrp' ? true : false"
@@ -130,15 +113,13 @@
             :precolnameslist="itemdata.precolnameslist"
             :colname="itemdata.colname"
             :itemdata="itemdata"
-            @pop="fktableShow"
-          />
-        <!-- </el-popover> -->
+            @pop="fktableShow" />
+          <!-- </el-popover> -->
         </Poptip>
       </div>
     </Tooltip>
 
-    <select-dialog
-      :title="SelectionData.item.name"
+    <select-dialog :title="SelectionData.item.name"
       :visible.sync="SingleSelect.show"
       :show-close="true"
       :select-config-list="SelectionData.config"
@@ -148,8 +129,7 @@
       @getChooseItem="getSelectChooseItem"
       @selectPageCurrentChange="selectPageCurrentChange"
       @selectPageSizeChange="selectPageSizeChange"
-      @selectionQueryTable="selectionQueryTable"
-    />
+      @selectionQueryTable="selectionQueryTable" />
   </div>
 </template>
 
