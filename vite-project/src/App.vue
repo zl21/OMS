217d271fd245b1ba028cd83094d8eb28eaaac115
zhou.l <zhou.l@burgeon.cn>
2022-06-02 14:05:25 +0800

    test: init project

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,11 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <Home/>
+
 </template>
 
 <style>
@@ -14,8 +17,11 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ vite-project/src/components/Home.vue
@@ -0,0 +1,125 @@
+
+<template>
+  <div class="home">
+    <div class="header">
+      <img src="../assets/logo1.png" alt="">
+    </div>
+    <div class="body">
+      <div class="body-left">
+        <div :key="index" v-for="(it,index) in info">
+          <span class="title" @click="titleClick(it)">{{it.en}}</span>
+          <span class="subTitle" @click="titleClick(it)"> - {{it.zh}}</span>
+        </div>
+      </div>
+      <div class=" body-right">
+        <!-- <img src="../assets/img1.png" alt=""> -->
+      </div>
+    </div>
+    <div class="footer">
+      <span class="copyright">©2021-2022 Burgeon Software Co.,Ltd. All rights reserved.</span>
+      <span class="author">BY OMS</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+
+defineProps<{ msg: string }>();
+
+const titleClick = (it) => {
+  window.open(it.url);
+  console.log("00000");
+};
+const count = ref(0);
+const info = reactive([
+  {
+    en: "OMS UI",
+    zh: "OMS组件库",
+    url: "http://101.132.182.36:20002/",
+  },
+  {
+    en: "THEME",
+    zh: "主题",
+    url: "http://101.132.182.36:20002/?path=/story/other-theme--page",
+  },
+  {
+    en: "I18N", // INTERNATIONALNATION
+    zh: "国际化",
+    url: "http://101.132.182.36:20000/",
+  },
+]);
+</script>
+
+<style lang="less" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  // font-size: 0;
+  padding: 40px 80px;
+  height: 100vh;
+  box-sizing: border-box;
+  .header {
+    img {
+      height: 60px;
+      float: left;
+      margin-left: 50px;
+    }
+  }
+  .body {
+    flex: 1;
+    display: flex;
+
+    &-left {
+      flex: 0.4;
+      div {
+        margin: 32px 0;
+      }
+      span {
+        display: block;
+        line-height: 1;
+        &:hover {
+          color: rgb(191, 191, 217);
+        }
+      }
+      .title {
+        font-size: 64px;
+        font-weight: 1000;
+        font-family: Arial Bold;
+        color: #f5f5f5;
+      }
+      .subTitle {
+        font-size: 24px;
+        font-weight: 500;
+        font-family: 微软雅黑;
+        // color: #f5f5f5;
+      }
+    }
+    &-right {
+      flex: 0.6;
+      width: 100%;
+      height: 100%;
+      background: url("../assets/img1.png") center / cover no-repeat;
+    }
+  }
+  .footer {
+    position: relative;
+    span {
+      position: absolute;
+      bottom: 0;
+      font-size: 12px;
+    }
+    // .copyright {
+    //   float: left;
+    // }
+    .author {
+      // float: right;
+      right: 0;
+    }
+  }
+  .header,
+  .footer {
+    flex: 0.2;
+  }
+}
+</style>
