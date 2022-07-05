7d10bc7701ebc97635e9df4366bd06e6595c7519
liushuanglian <li.sl@burgeon.cn>
2022-07-05 18:41:33 +0800

    fix: 零售 - 是否显示折叠按钮

--- src/js/OmsForm.js
+++ src/js/OmsForm.js
@@ -86,7 +86,13 @@ export default {
     },
     // 判断是否显示折叠按钮（判断查询条件是否 大于 查询条件默认显示行数*一行显示条数，大于才显示折叠按钮 ）
     searchInputLenMoreThanShowNum() {
-      return this.formConfig.formData.length > this.showNum
+      const hasMore = this.formConfig.formData.length > this.showNum
+      if (!hasMore) {
+        $('.fold-btn').addClass('hide-top-fold-btn');
+      } else {
+        $('.fold-btn').removeClass('hide-top-fold-btn');
+      }
+      return hasMore
     }
   },
   watch: {
