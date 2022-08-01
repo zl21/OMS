d3bab9884abbe993243efe39079668bb75570597
zhou.l <zhou.l@burgeon.cn>
2022-08-01 18:01:55 +0800

    docs: mini.md

--- run.js
+++ run.js
@@ -7,5 +7,7 @@ const compiler = webpack(config)
 
 // 调用，打包
 compiler.run((err, status) => {
-  console.log('run::', err, status)
+  console.log('run::', err, status.toJson()) // 自带 toJson 方法
 })
+
+// 执行 node run.js
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   devtool: false,
   mode: 'development',
+  context: path.resolve(__dirname, ''), // 指定打包项目的起点 即 根目录
   entry: './src/index.js',
   output: {
     filename: 'main.js',
