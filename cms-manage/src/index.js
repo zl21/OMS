4825ef8c9287c94774f69252d7ab28dbc8843b32
zhou.l <zhou.l@burgeon.cn>
2022-08-22 15:13:15 +0800

    test: ReactDOM.render

--- cms-manage/src/index.js
+++ cms-manage/src/index.js
@@ -16,6 +16,23 @@ root.render(
   </React.StrictMode>
 );
 
+
+/* 
+// 也可以用如下写法，有警告：
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  // Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot
+  <React.StrictMode>
+    <Provider store={store}>
+      <App1 />
+    </Provider>
+  </React.StrictMode>
+  ,
+  document.getElementById('root')
+)
+ */
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
